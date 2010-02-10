30b8130d2a67c2ad528ee753d5d206d82b3c43eb
argriffi <argriffi@zephyr.statgen.ncsu.edu>
2010-02-10 10:57:14 -0500

    redo a bit of the interface to the jukes cantor module

--- JC69.py
+++ JC69.py
@@ -11,6 +11,25 @@ import random
 import MatrixUtil
 import Util
 
+def distance_to_probability(d):
+    """
+    @param d: the expected number of substitutions along a branch
+    @return: the probability that the endpoints are different
+    """
+    return (3.0 / 4.0) * (1.0 - math.exp(-(4.0 / 3.0) * d))
+
+def probability_to_distance(p):
+    """
+    Note that p can also be an observed proportion of nucleotide differences.
+    In this case the returned value is the maximum likelihood estimate
+    of the expected number of substitutions along the branch.
+    @param p: the probability that the endpoints of a branch are different
+    @return: the expected number of substitutions along the branch
+    """
+    if p >= (3.0 / 4.0):
+        return float('inf')
+    return -(3.0 / 4.0) * math.log(1.0 - (4.0 / 3.0) * p)
+
 def sample_xtree_sequences(root, sequence_length):
     """
     @param root: the root of a phylogenetic xtree with branch lengths
@@ -115,17 +134,29 @@ def get_ML_distance_matrix(sequence_list):
 
 
 class TestJC69(unittest.TestCase):
+
+    def test_conversion_special_cases(self):
+        d = probability_to_distance(0.0)
+        self.assertAlmostEqual(d, 0)
+        d = probability_to_distance(3.0 / 4.0)
+        self.assertEqual(d, float('inf'))
     
-    def test_placeholder(self):
-        pass
+    def test_conversion(self):
+        probs = (0.0, 0.2, 0.4, 0.6)
+        for p_expected in probs:
+            d = probability_to_distance(p_expected)
+            p_observed = distance_to_probability(d)
+            self.assertAlmostEqual(p_expected, p_observed)
+
+    def test_ml(self):
+        sa = 'AAAAA'
+        sbs = ('AAAAA', 'AAAAC', 'AAACC', 'AACCC')
+        probs = (0.0, 0.2, 0.4, 0.6)
+        for p_expected, sb in zip(probs, sbs):
+            d_expected = probability_to_distance(p_expected)
+            d_observed = get_ML_distance(sa, sb)
+            self.assertAlmostEqual(d_expected, d_observed)
 
 
 if __name__ == '__main__':
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.add_option('--test', action='store_true', dest='test', default=False)
-    options, args = parser.parse_args()
-    if options.test:
-        suite = unittest.TestLoader().loadTestsFromTestCase(TestJC69)
-        unittest.TextTestRunner(verbosity=2).run(suite)
-
+    unittest.main()
