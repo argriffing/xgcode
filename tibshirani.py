305561887ca50f89fe24cc1229d5728364c6aed3
alex griffing <argriffi@ncsu.edu>
2010-07-09 07:28:27 -0400

    add helper functions for computing the tibshirani gap statistic

--- tibshirani.py
+++ tibshirani.py
@@ -1,16 +1,70 @@
 """
 Implmentation of the gap statistic for clustering.
 
-This is implemented from the description in the paper entitled
-estimating the number of clusters in a data set via the gap statistic.
+This is implemented from the description
+in the paper entitled 'estimating the number of clusters
+in a data set via the gap statistic' by Tibshirani et al.
 """
 
 import unittest
 import itertools
+import random
 
 import numpy as np
 
-def get_sum_of_distances_fast(X):
+def points_to_ranges(X):
+    """
+    @param X: each row of numpy array X is a point
+    @return: a list giving, for each coordinate, a pair of low, high bounds
+    """
+    lows = np.min(X, axis=0)
+    highs = np.max(X, axis=0)
+    return zip(lows.tolist(), highs.tolist())
+
+def sample_points(ranges, n):
+    """
+    @param ranges: for each coordinate, a pair of low, high bounds
+    @param n: the number of points to sample
+    @return: a 2D numpy array where each row is a point
+    """
+    return np.array([_sample_point(ranges) for i in range(n)])
+
+def get_simulation_correction(wlogs):
+    """
+    The input is the array of logs of wcss values from the simulations.
+    This is defined in the third step of the computation of the
+    gap statistic in section four of the gap statistic paper.
+    @param wlogs: numpy array of logs of within cluster sums of squares
+    @return: the correction for simulation error
+    """
+    B = len(ws)
+    sd = np.std(wlogs)
+    return sd * math.sqrt(1.0 + 1.0/B)
+
+def get_wcss(X):
+    """
+    @param X: each row of numpy array X is a point
+    @return: sum of squares of distances to the center
+    """
+    nrows, ncols = X.shape
+    center = X.mean(axis=0)
+    return np.sum((X-center)**2)
+
+def get_sum_of_distances(X):
+    """
+    @param X: each row of numpy array X is a point
+    @return: sum of euclidean distances between all ordered pairs
+    """
+    return _get_sum_of_distances_fast(X)
+
+def _sample_point(ranges):
+    """
+    @param ranges: for each coordinate, a pair of low, high bounds
+    @return: a float sequence
+    """
+    return [random.uniform(low, high) for low, high in ranges]
+
+def _get_sum_of_distances_fast(X):
     """
     @param X: each row of numpy array X is a point
     @return: sum of euclidean distances between all ordered pairs
@@ -21,7 +75,7 @@ def get_sum_of_distances_fast(X):
     beta = np.dot(colsums, colsums)
     return 2*(alpha - beta)
 
-def get_sum_of_distances_centered(X):
+def _get_sum_of_distances_centered(X):
     """
     @param X: each row of numpy array X is a point
     @return: sum of euclidean distances between all ordered pairs
@@ -30,7 +84,7 @@ def get_sum_of_distances_centered(X):
     center = X.mean(axis=0)
     return 2*nrows*np.sum((X-center)**2)
 
-def get_sum_of_distances_naive(X):
+def _get_sum_of_distances_naive(X):
     """
     @param X: each row of numpy array X is a point
     @return: sum of euclidean distances between all ordered pairs
@@ -48,13 +102,13 @@ class TestTibshirani(unittest.TestCase):
             [8, 2, 3, 6]], dtype=float)
     
     def test_sum_of_distances_fast(self):
-        expected = get_sum_of_distances_naive(self.M)
-        observed = get_sum_of_distances_fast(self.M)
+        expected = _get_sum_of_distances_naive(self.M)
+        observed = _get_sum_of_distances_fast(self.M)
         self.assertTrue(np.allclose(expected, observed))
     
     def test_sum_of_distances_centered(self):
-        expected = get_sum_of_distances_naive(self.M)
-        observed = get_sum_of_distances_centered(self.M)
+        expected = _get_sum_of_distances_naive(self.M)
+        observed = _get_sum_of_distances_centered(self.M)
         self.assertTrue(np.allclose(expected, observed))
 
 if __name__ == '__main__':
