7fa68f6d79bfd390afbe9335d94266c037d9c19d
alex griffing <argriffi@ncsu.edu>
2010-07-11 15:24:55 -0400

    begin code that writes mobyle xml interfaces

---
+++ FormOut.py
@@ -0,0 +1,48 @@
+"""
+This is how the output format of a snippet is specified.
+
+Some metaprogramming is done for compatibility with Mobyle.
+In Mobyle, all script output is to a file or to stdout.
+Also stdout is treated as a file.
+In my more constrained system, script output is only to stdout.
+"""
+
+import Form
+
+
+class FormOut(object):
+
+    def __init__(self, filename_format_string, filename_interpolants):
+        self.filename_format_string = filename_format_string
+        self.filename_interpolants = filename_interpolants
+
+    def get_filename_code(self):
+        """
+        This is ugly metaprogramming for Mobyle compatibility.
+        @return: a string that is a python expression
+        """
+        if len(self.filename_interpolants) == 1:
+            return '"%s" %% %s' % (
+                    self.filename_format_string,
+                    self.filename_interpolants[0])
+        elif len(self.filename_interpolants) > 1:
+            return '"%s" %% (%s)' % (
+                    self.filename_format_string,
+                    ', '.join(self.filename_interpolants))
+        else:
+            return self.filename_format_string
+
+    def get_filename(self, fs):
+        """
+        @param fs: a fieldstorage object
+        """
+        rhs = tuple(getattr(fs, x) for x in self.filename_interpolants)
+        return self.filename_format_string % rhs
+
+    def get_content_type(self, fs):
+        return 'text/plain'
+
+
+class Image(FormOut):
+
+    def get_content_type(self, fs):
---
+++ create-mob-xmls.py
@@ -0,0 +1,34 @@
+"""
+Create Mobyle-compatible xml interfaces.
+"""
+
+import os
+
+import argparse
+
+import mobyle
+
+
+def main(args):
+    with open(args.manifest) as fin:
+        module_names = [x.strip() for x in fin]
+    for name in module_names:
+        try:
+            xml_content = mobyle.get_xml(args.auto, name)
+        except mobyle.MobyleError as e:
+            xml_content = None
+            print e
+        if xml_content:
+            xml_filename = os.path.join(target, 'z' + name + '.xml')
+            with open(xml_filename) as fout:
+                fout.write(xml_content)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--manifest', required=True,
+            help='a module manifest filename')
+    parser.add_argument('--target', required=True,
+            help='files will be created in this existing directory')
+    parser.add_argument('--auto', required=True,
+            help='path to auto.py')
+    main(parser.parse_args())
