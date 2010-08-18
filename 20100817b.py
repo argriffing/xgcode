f0ff69c977ad9f0cdd3b3e1228e6f06a8db7b3ba
alex griffing <argriffi@ncsu.edu>
2010-08-18 16:16:49 -0400

    add colors to the minimum spanning tree

--- 20100817b.py
+++ 20100817b.py
@@ -3,6 +3,7 @@
 
 import random
 import math
+import itertools
 
 import numpy as np
 import cairo
@@ -13,9 +14,10 @@ import Form
 import FormOut
 import CompGeom
 import CairoUtil
+import EigUtil
+import MST
 import const
-
-g_tree_string = const.read('20100730g').rstrip()
+import Euclid
 
 g_africa_poly = [
         (686., 274.),
@@ -50,7 +52,9 @@ def get_form():
     """
     # define the form objects
     form_objects = [
+            Form.Integer('seed', 'rng seed or 0 for no fixed seed', 0),
             Form.Integer('npoints', 'sample this many points', 200),
+            Form.Integer('axis', 'color according to this MDS axis', 0),
             Form.Integer('total_width', 'total image width',
                 640, low=3, high=2000),
             Form.Integer('total_height', 'total image height',
@@ -102,8 +106,46 @@ def gen_noncrossing_edges(query_edges, boundary_edges, allpoints):
         else:
             yield query
 
+def get_mst(query_edges, allpoints):
+    """
+    Return a sublist of the query edges which forms a minimum spanning tree.
+    @param query_edges: point index pairs
+    @param allpoints: ordered points as pairs of coordinates
+    @return: minimum spanning tree edges
+    """
+    # get the point indices occurring in at least one query edge
+    V = list(sorted(set(itertools.chain.from_iterable(query_edges))))
+    # get the weighted edges
+    E = []
+    for edge in query_edges:
+        ax, ay = allpoints[edge[0]]
+        bx, by = allpoints[edge[1]]
+        weight = math.hypot(bx-ax, by-ay)
+        E.append((weight, edge[0], edge[1]))
+    # get the minimum spanning tree weighted edges
+    E_mst = MST.kruskal(V, E)
+    # get the minimum spanning tree edges
+    return [(va, vb) for w, va, vb in E_mst]
+
+def get_color(x):
+    """
+    @param x: between -1 and 1
+    @return: an rgb triple in [0,1]
+    """
+    r = 1.0 if x > 0 else 1.0 - abs(x)
+    g = 1.0 if x < 0 else 1.0 - abs(x)
+    b = 1.0 - abs(x)
+    return r, g, b
+
 def get_response_content(fs):
+    # use a fixed seed if requested
+    if fs.seed:
+        random.seed(fs.seed)
+    # define the max number of rejection iterations
     limit = fs.npoints*100
+    # validate input
+    if fs.axis < 0:
+        raise ValueError('the mds axis must be nonnegative')
     # get points defining the boundary of africa
     nafrica = len(g_africa_poly)
     africa_edges = [(i, (i+1)%nafrica) for i in range(nafrica)]
@@ -116,7 +158,25 @@ def get_response_content(fs):
     allpoints = g_africa_poly + points
     # refine the list of edges
     tri_edges = list(gen_noncrossing_edges(tri_edges, africa_edges, allpoints))
+    tri_edges = get_mst(tri_edges, allpoints)
     alledges = africa_edges + tri_edges
+    # make the graph laplacian
+    A = np.zeros((len(points), len(points)))
+    for ia, ib in tri_edges:
+        xa, ya = allpoints[ia]
+        xb, yb = allpoints[ib]
+        d = math.hypot(xb-xa, yb-ya)
+        A[ia-nafrica, ib-nafrica] = 1/d
+        A[ib-nafrica, ia-nafrica] = 1/d
+    L = Euclid.adjacency_to_laplacian(A)
+    ws, vs = EigUtil.eigh(np.linalg.pinv(L))
+    if fs.axis >= len(ws):
+        raise ValueError('choose a smaller mds axis')
+    v = vs[fs.axis]
+    # get the color and sizes for the points
+    v /= max(np.abs(v))
+    colors = [(0,0,0)]*nafrica + [get_color(x) for x in v]
+    radii = [2]*nafrica + [5 for p in points]
     # get the width and height of the drawable area of the image
     width = fs.total_width - 2*fs.border
     height = fs.total_height - 2*fs.border
@@ -124,69 +184,86 @@ def get_response_content(fs):
         msg = 'the image dimensions do not allow for enough drawable area'
         raise HandlingError(msg)
     # draw the image
+    ext = Form.g_imageformat_to_ext[fs.imageformat]
     try:
-        ext = Form.g_imageformat_to_ext[fs.imageformat]
-        return get_image_string(allpoints, alledges,
-                fs.total_width, fs.total_height, fs.border, ext)
+        helper = ImgHelper(allpoints, alledges,
+                fs.total_width, fs.total_height, fs.border)
+        return helper.get_image_string(colors, radii, ext)
     except CairoUtil.CairoUtilError, e:
         raise HandlingError(e)
 
-def get_image_string(points, edges, t_width, t_height, border, image_format):
+
+class ImgHelper:
     """
-    @param points: an ordered list of (x, y) pairs
-    @param edges: a set of point index pairs
-    @param t_width: the image width in pixels
-    @param t_height: the image height in pixels
-    @param border: the width and height of the image border in pixels
-    @param image_format: the requested format of the image
+    This used to be a single function but there is too much going on in it.
     """
-    width = t_width - 2*border
-    height = t_height - 2*border
-    assert width >= 1
-    assert height >= 1
-    # get the x and y coordinates of the points
-    x_coords, y_coords = zip(*points)
-    unscaled_width = max(x_coords) - min(x_coords)
-    unscaled_height = max(y_coords) - min(y_coords)
-    # rescale the points to fit in the drawable part of the image
-    c_width = width / unscaled_width
-    c_height = height / unscaled_height
-    c = min(c_width, c_height)
-    x_rescaled = [x*c for x in x_coords]
-    y_rescaled = [y*c for y in y_coords]
-    # translate the points so that their minimum coordinate is zero
-    x_min = min(x_rescaled)
-    y_min = min(y_rescaled)
-    x_trans = [x-x_min for x in x_rescaled]
-    y_trans = [y-y_min for y in y_rescaled]
-    # translate the points to account for the border
-    x_final = [x+border for x in x_trans]
-    y_final = [y+border for y in y_trans]
-    # create the surface
-    cairo_helper = CairoUtil.CairoHelper(image_format)
-    surface = cairo_helper.create_surface(t_width, t_height)
-    context = cairo.Context(surface)
-    # draw the background
-    context.save()
-    context.set_source_rgb(.9, .9, .9)
-    context.paint()
-    context.restore()
-    # draw the points
-    radius = 2.0
-    for x, y in zip(x_final, y_final):
-        # draw a filled circle
+
+    def __init__(self, points, edges, t_width, t_height, border):
+        """
+        @param points: an ordered list of (x, y) pairs
+        @param edges: a set of point index pairs
+        @param t_width: the image width in pixels
+        @param t_height: the image height in pixels
+        @param border: the width and height of the image border in pixels
+        """
+        self.edges = edges
+        self.t_width = t_width
+        self.t_height = t_height
+        width = t_width - 2*border
+        height = t_height - 2*border
+        assert width >= 1
+        assert height >= 1
+        # get the x and y coordinates of the points
+        x_coords, y_coords = zip(*points)
+        unscaled_width = max(x_coords) - min(x_coords)
+        unscaled_height = max(y_coords) - min(y_coords)
+        # rescale the points to fit in the drawable part of the image
+        c_width = width / unscaled_width
+        c_height = height / unscaled_height
+        c = min(c_width, c_height)
+        x_rescaled = [x*c for x in x_coords]
+        y_rescaled = [y*c for y in y_coords]
+        # translate the points so that their minimum coordinate is zero
+        x_min = min(x_rescaled)
+        y_min = min(y_rescaled)
+        x_trans = [x-x_min for x in x_rescaled]
+        y_trans = [y-y_min for y in y_rescaled]
+        # translate the points to account for the border
+        self.x_final = [x+border for x in x_trans]
+        self.y_final = [y+border for y in y_trans]
+
+    def get_image_string(self, colors, radii, image_format):
+        """
+        @param colors: point colors as rgb triples in [0,1]
+        @param radii: point radii should be about 2.0
+        @param image_format: the requested format of the image
+        @return: the image string
+        """
+        # create the surface
+        cairo_helper = CairoUtil.CairoHelper(image_format)
+        surface = cairo_helper.create_surface(self.t_width, self.t_height)
+        context = cairo.Context(surface)
+        # draw the background
         context.save()
-        context.arc(x, y, radius, 0, 2 * math.pi)
-        context.close_path()
-        context.fill()
+        context.set_source_rgb(.9, .9, .9)
+        context.paint()
         context.restore()
-    # draw the edges
-    for i, j in edges:
-        context.save()
-        context.move_to(x_final[i], y_final[i])
-        context.line_to(x_final[j], y_final[j])
-        context.close_path()
-        context.stroke()
-        context.restore()
-    # get the image string
-    return cairo_helper.get_image_string()
+        # draw the edges
+        for i, j in self.edges:
+            context.save()
+            context.move_to(self.x_final[i], self.y_final[i])
+            context.line_to(self.x_final[j], self.y_final[j])
+            context.close_path()
+            context.stroke()
+            context.restore()
+        # draw the points
+        for r, c, x, y in zip(radii, colors, self.x_final, self.y_final):
+            # draw a filled circle
+            context.save()
+            context.set_source_rgb(*c)
+            context.arc(x, y, r, 0, 2 * math.pi)
+            context.close_path()
+            context.fill()
+            context.restore()
+        # get the image string
+        return cairo_helper.get_image_string()
