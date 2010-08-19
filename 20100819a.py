99c96c494396e7e6f83ea4866fed941759a33b91
alex griffing <argriffi@ncsu.edu>
2010-08-19 16:22:13 -0400

    the numerical experiment for matrix products in the limit of the augmented matrices is now useful

--- 20100819a.py
+++ 20100819a.py
@@ -34,6 +34,28 @@ def get_form():
 def get_form_out():
     return FormOut.Report()
 
+def get_alpha_multiplier(D, ntips):
+    n = len(D)
+    q = ntips
+    p = n - ntips
+    Xt = D[p:].T[:p].T
+    Dq = D[p:].T[p:].T
+    uno = np.ones((q,p), dtype=float)
+    right = np.dot(Dq, uno)/q
+    R = Xt - right
+    return R - np.mean(R, axis=0)
+
+def get_beta_multiplier(D, ntips):
+    n = len(D)
+    q = ntips
+    p = n - ntips
+    X = D[p:].T[:p]
+    Dq = D[p:].T[p:].T
+    uno = np.ones((p,q), dtype=float)
+    right = np.dot(uno, Dq)/q
+    R = X - right
+    return (R.T - np.mean(R.T, axis=0)).T
+
 def get_response_content(fs):
     # build the newick tree from the string
     tree = NewickIO.parse(fs.tree_string, FelTree.NewickTree)
@@ -46,10 +68,9 @@ def get_response_content(fs):
     # get the distance matrix and the augmented distance matrix
     D_leaf = np.array(tree.get_partial_distance_matrix(leaf_ids))
     D = np.array(tree.get_partial_distance_matrix(ordered_ids))
-    D_aug = get_augmented_distance(D, nleaves, fs.ndups)
     # analyze the leaf distance matrix
     X_leaf = Euclid.edm_to_points(D_leaf)
-    w_leaf, v_leaf = Euclid.edm_to_dccov(D_leaf)
+    w_leaf, v_leaf = EigUtil.eigh(Euclid.edm_to_dccov(D_leaf))
     V_leaf = np.array(v_leaf).T
     # explicitly compute the limiting points as the number of dups increases
     X = Euclid.edm_to_points(D)
@@ -57,9 +78,16 @@ def get_response_content(fs):
     XL = X[-nleaves:]
     U, s, Vt = np.linalg.svd(XL)
     Z = np.dot(X, Vt.T)
-    # hack the Z matrix
-    Z = Z.T[ninternal:].T
-    WY = Z / np.sqrt(w_leaf)
+    # hack the Z matrix to show the leaf-related eigenvectors
+    Z = Z.T[:nleaves-1].T
+    WY = Z / np.sqrt(w_leaf[:-1])
+    # compute a product using the first few rows of WY
+    W = WY[:ninternal]
+    M_alpha = get_alpha_multiplier(D, nleaves)
+    MW_alpha = np.dot(M_alpha, W)
+    # compute a product using the first few rows of WY
+    M_beta = get_beta_multiplier(D, nleaves)
+    MY_beta = np.dot(M_beta, V_leaf)
     # report the results
     np.set_printoptions(linewidth=300, threshold=10000)
     out = StringIO()
@@ -72,8 +100,26 @@ def get_response_content(fs):
     print >> out, 'corresponding eigenvectors (as columns)'
     print >> out, V_leaf
     print >> out
-    print >> out, 'limiting points:'
+    print >> out, "candidates for [W' Y']':"
     print >> out, WY
+    print >> out
+    print >> out, 'candidates for W:'
+    print >> out, W
+    print >> out
+    print >> out, 'left multiplier of W:'
+    print >> out, M_alpha
+    print >> out
+    print >> out, 'each column is a (left multiplier, W) product:'
+    print >> out, MW_alpha
+    print >> out
+    print >> out, 'left multiplier of Y:'
+    print >> out, M_beta
+    print >> out
+    print >> out, 'each column is a (left multiplier, Y) product:'
+    print >> out, MY_beta
+    print >> out
+    print >> out, 'the above matrix divided by 2*eigenvalue:'
+    print >> out, MY_beta / (2*np.array(w_leaf))
     print >> out
     return out.getvalue()
 
