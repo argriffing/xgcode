cd7f65b5abc5b31fb5f866950181b256d530bad1
a g <argriffi@brcdesk18.(none)>
2011-01-10 14:58:59 -0500

    remove an unused import and reformat the code to fit the column limit

--- 20091103a.py
+++ 20091103a.py
@@ -34,7 +34,6 @@ import Form
 import FormOut
 import NewickIO
 import Euclid
-import NeighborJoining
 import FelTree
 import CairoUtil
 import Progress
@@ -86,27 +85,32 @@ def get_response_content(fs):
 
 def reflect_to_reference(points, reference_points):
     """
-    For 2D points, try each of the 4 possible combinations reflections across the axes.
+    For 2D points, try each combination of reflections across the axes.
+    There are four possible combinations of reflections.
     Use the reflection that gives points closest to the reference points.
     @param points: rows are 2D points
     @param reference_points: rows are 2D points
     @return: 
     """
     if points.shape != reference_points.shape:
-        raise ValueError('the point array and the reference point array should have the same shape')
+        msg_a = 'the point array and the reference point array '
+        msg_b = 'should have the same shape'
+        raise ValueError(msg_a + msg_b)
     if len(points.shape) != 2:
-        raise ValueError('the points argument should be a matrix-like numpy array')
+        msg = 'the points argument should be a matrix-like numpy array'
+        raise ValueError(msg)
     if points.shape[1] != 2:
         raise ValueError('the points should be in 2D space')
     reflectors = np.array(list(product((-1,1), repeat=2)))
-    best_error, best_reflector = min((np.linalg.norm(points*r - reference_points), r) for r in reflectors)
+    best_error, best_reflector = min((np.linalg.norm(
+        points*r - reference_points), r) for r in reflectors)
     return points * best_reflector
 
 def get_index_edges(tree, ordered_ids):
     """
-    Given a tree and some ordered ids, get a collection of edges defined on indices.
+    Given a tree and some ordered ids, get edges defined on indices.
     @param tree: the tree object
-    @param ordered_ids: the returned index pairs are with respect to this sequence
+    @param ordered_ids: the returned index pairs are for this sequence
     @return: a collection of index pairs defining edges
     """
     # map ids to indices
@@ -122,12 +126,14 @@ def get_index_edges(tree, ordered_ids):
 
 def get_mass_vector(nvertices, nleaves, progress):
     """
-    @param nvertices: the number of vertices in the tree, including internal vertices
+    The progress parameter goes from uniform weights to uniform tip weights.
+    @param nvertices: the number of vertices in the full tree
     @param nleaves: the number of leaves in the tree
-    @param progress: between 0.0 (uniform weights) and 1.0 (uniform tip weights, zero internal weights)
+    @param progress: progress in [0.0, 1.0]
     @return: a nonnegative mass vector that sums to 1.0 for weighting the MDS
     """
-    # tips get weights proportional to 1.0 and internal vertices get weights proportional to (1-progress)
+    # tips get weights proportional to 1.0
+    # and internal vertices get weights proportional to (1-progress)
     mass_vector = np.ones(nvertices, dtype=float)
     for i in range(nleaves, nvertices):
         mass_vector[i] = 1-progress
@@ -146,7 +152,8 @@ def get_canonical_2d_mds(D, m, reference_points):
     X = Euclid.edm_to_weighted_points(D, m)
     return reflect_to_reference(X.T[:2].T, reference_points)
 
-def get_animation_frame(image_format, physical_size, scale, mass_vector, index_edges, points):
+def get_animation_frame(
+        image_format, physical_size, scale, mass_vector, index_edges, points):
     """
     This function is about drawing the tree.
     @param image_format: the image extension
@@ -191,7 +198,8 @@ def get_animation_frame(image_format, physical_size, scale, mass_vector, index_e
         context.line_to(x0 + bx*scale, y0 + by*scale)
         context.stroke()
     context.restore()
-    # draw vertices as translucent circles with radius defined by the mass vector
+    # Draw vertices as translucent circles
+    # with radius defined by the mass vector.
     context.save()
     context.set_source_rgba(0.2, 0.2, 1.0, 0.5)
     for point, mass in zip(points, mass_vector):
@@ -231,11 +239,13 @@ def main(args):
     tree = NewickIO.parse(args.tree, FelTree.NewickTree)
     nvertices = len(list(tree.preorder()))
     nleaves = len(list(tree.gen_tips()))
-    # get ordered ids with the leaves first, and get the corresponding distance matrix
+    # Get ordered ids with the leaves first,
+    # and get the corresponding distance matrix.
     ordered_ids = get_ordered_ids(tree)
     D = np.array(tree.get_partial_distance_matrix(ordered_ids))
     index_edges = get_index_edges(tree, ordered_ids)
-    # create the reference points so that the video frames are not reflected arbitrarily
+    # Create the reference points
+    # so that the video frames are not reflected arbitrarily.
     reference_points = Euclid.edm_to_points(D).T[:2].T
     # create the animation frames and write them as image files
     pbar = Progress.Bar(args.nframes)
@@ -247,7 +257,9 @@ def main(args):
             progress = linear_progress
         mass_vector = get_mass_vector(nvertices, nleaves, progress)
         points = get_canonical_2d_mds(D, mass_vector, reference_points)
-        image_string = get_animation_frame(args.image_format, physical_size, args.scale, mass_vector, index_edges, points)
+        image_string = get_animation_frame(
+                args.image_format, physical_size, args.scale,
+                mass_vector, index_edges, points)
         image_filename = 'frame-%04d.%s' % (frame_index, args.image_format)
         image_pathname = os.path.join(args.output_directory, image_filename)
         with open(image_pathname, 'wb') as fout:
@@ -257,13 +269,23 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--scale', type=float, default=1.0, help='define the drawing scale') 
-    parser.add_argument('--physical_width', type=int, default=480, help='width (pixels)') 
-    parser.add_argument('--physical_height', type=int, default=360, help='height (pixels)') 
-    parser.add_argument('--tree', default=g_tree_string, help='newick tree with branch lengths')
-    parser.add_argument('--image_format', default='png', choices=('png', 'svg', 'ps', 'pdf'), help='image format')
-    parser.add_argument('--nframes', type=int, default=100, help='number of animation frames (image files) to create') 
-    parser.add_argument('--interpolation', default='sigmoid', choices=('sigmoid', 'linear'), help='weights change according to this function')
-    parser.add_argument('output_directory', help='path to the directory to which the .png frames should be written')
+    parser.add_argument('--scale', type=float, default=1.0,
+            help='define the drawing scale') 
+    parser.add_argument('--physical_width', type=int, default=480,
+            help='width (pixels)') 
+    parser.add_argument('--physical_height', type=int, default=360,
+            help='height (pixels)') 
+    parser.add_argument('--tree', default=g_tree_string,
+            help='newick tree with branch lengths')
+    parser.add_argument('--image_format', default='png',
+            choices=('png', 'svg', 'ps', 'pdf'),
+            help='image format')
+    parser.add_argument('--nframes', type=int, default=100,
+            help='number of animation frames (image files) to create') 
+    parser.add_argument('--interpolation', default='sigmoid',
+            choices=('sigmoid', 'linear'),
+            help='weights change according to this function')
+    parser.add_argument('output_directory',
+            help='path to the output directory for .png frames')
     main(parser.parse_args())
 
