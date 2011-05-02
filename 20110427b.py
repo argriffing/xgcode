c8d51c1fb2b086a2a1d41d2325ded3d8502f2e05
a g <argriffi@brcdesk18.(none)>
2011-05-02 14:28:03 -0400

    reformat the default newick string of another snippet so that it uses a multi-line format

--- 20110427b.py
+++ 20110427b.py
@@ -9,6 +9,7 @@ import Form
 import FormOut
 import Ftree
 import FtreeIO
+import Newick
 
 g_default_tree = '(((3:0.333333333333, 4:0.5)7:1.0, 5:1.0)8:0.088383145868, (1:1.0, 2:0.5)6:0.911616854132)r;'
 
@@ -16,9 +17,12 @@ def get_form():
     """
     @return: the body of a form
     """
+    # format the default tree string
+    tree = Newick.parse(g_default_tree, Newick.NewickTree)
+    formatted_tree_string = Newick.get_narrow_newick_string(tree, 60)
     # define the form objects
     form_objects = [
-            Form.MultiLine('tree', 'tree', g_default_tree)]
+            Form.MultiLine('tree', 'tree', formatted_tree_string)]
     return form_objects
 
 def get_form_out():
