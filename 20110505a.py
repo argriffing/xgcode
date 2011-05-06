acda8787cf37566457358b62a064f8d345488bdc
a g <argriffi@brcdesk18.(none)>
2011-05-06 15:57:23 -0400

    add an animation snippet

---
+++ 20110505a.py
@@ -0,0 +1,159 @@
+"""
+Animate a 3D tree.
+
+To convert a sequence of png images to an mpeg video:
+ffmpeg -i frames/frame-%04d.png test.mpg
+Resolutions preferred by YouTube are 640x360 and 480x360. 
+"""
+
+
+from StringIO import StringIO
+import os
+import math
+
+import numpy as np
+import scipy.stats
+import cairo
+import argparse
+
+import Form
+import FormOut
+import Ftree
+import FtreeIO
+import Euclid
+import CairoUtil
+import Progress
+import const
+import TreeProjection
+import NewickIO
+
+#g_tree_string = const.read('20100730g').rstrip()
+g_tree_string = NewickIO.daylight_example_tree
+
+g_default_yaw = 0
+g_default_pitch = 0.2
+g_default_eigenvector_index = 2
+
+def get_form():
+    """
+    @return: a list of form objects
+    """
+    # define the form objects
+    form_objects = [
+            Form.MultiLine('tree_string', 'newick tree',
+                g_tree_string),
+            #Form.Float('scale', 'scale the image of the tree by this factor',
+            #200.0, low_exclusive=0.0),
+            Form.Integer('eigenvector_index',
+                'eigenvector index (1 is Fiedler)',
+                g_default_eigenvector_index, low=1),
+            Form.Float('yaw', 'yaw', g_default_yaw),
+            Form.Float('pitch', 'pitch', g_default_pitch),
+            Form.ImageFormat(),
+            Form.ContentDisposition()]
+    return form_objects
+
+def get_form_out():
+    return FormOut.Image('frame')
+
+def get_response_content(fs):
+    """
+    @param fs: a FieldStorage object containing the cgi arguments
+    @return: the response
+    """
+    physical_size = (640, 480)
+    scale = 10.0
+    return get_animation_frame(
+            fs.imageformat, physical_size, scale,
+            fs.tree_string, fs.eigenvector_index, fs.yaw, fs.pitch)
+
+def t_to_yaw(t):
+    """
+    @param t: animation progress between 0 and 1
+    @return: yaw
+    """
+    yaw = math.pi*2*t
+    return yaw
+
+def t_to_pitch(t):
+    """
+    @param t: animation progress between 0 and 1
+    @return: pitch
+    """
+    pitch = (math.pi / 4.0) * math.sin(math.pi * 2.0 * 3 * t)
+    return pitch
+
+def get_animation_frame(
+        image_format, physical_size, scale,
+        newick, eigenvector_index, yaw, pitch):
+    """
+    This function is about drawing the tree.
+    @param image_format: the image extension
+    @param physical_size: the width and height of the image in pixels
+    @param scale: a scaling factor
+    @return: the animation frame as an image as a string
+    """
+    # before we begin drawing we need to create the cairo surface and context
+    cairo_helper = CairoUtil.CairoHelper(image_format)
+    surface = cairo_helper.create_surface(physical_size[0], physical_size[1])
+    ctx = cairo.Context(surface)
+    # draw a white background 
+    ctx.save() 
+    ctx.set_source_rgb(1, 1, 1)
+    ctx.paint() 
+    ctx.restore() 
+    # define some helper variables
+    x0 = physical_size[0] / 2.0
+    y0 = physical_size[1] / 2.0
+    # translate to the center of the frame
+    ctx.translate(x0, y0)
+    ctx.scale(1, -1)
+    # draw the info
+    TreeProjection.draw_cairo_frame(
+            ctx, scale, newick, eigenvector_index, yaw, pitch)
+    # create the image
+    return cairo_helper.get_image_string()
+
+def main(args):
+    # do some validation
+    if args.nframes < 2:
+        raise ValueError('nframes should be at least 2')
+    # define the requested physical size of the images (in pixels)
+    physical_size = (args.physical_width, args.physical_height)
+    # create the animation frames and write them as image files
+    pbar = Progress.Bar(args.nframes)
+    for frame_index in range(args.nframes):
+        t = frame_index / float(args.nframes - 1)
+        image_string = get_animation_frame(
+                args.image_format, physical_size, args.scale,
+                args.tree, args.eigenvector_index,
+                t_to_yaw(t), t_to_pitch(t))
+        image_filename = 'frame-%04d.%s' % (frame_index, args.image_format)
+        image_pathname = os.path.join(args.output_directory, image_filename)
+        with open(image_pathname, 'wb') as fout:
+            fout.write(image_string)
+        pbar.update(frame_index+1)
+    pbar.finish()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--scale', type=float, default=10.0,
+            help='define the drawing scale') 
+    parser.add_argument('--physical_width', type=int, default=480,
+            help='width (pixels)') 
+    parser.add_argument('--physical_height', type=int, default=360,
+            help='height (pixels)') 
+    parser.add_argument('--tree', default=g_tree_string,
+            help='newick tree with branch lengths')
+    parser.add_argument('--eigenvector_index',
+            default=g_default_eigenvector_index, type=int,
+            help='eigenvector index (1 is Fiedler)')
+    parser.add_argument('--image_format', default='png',
+            choices=('png', 'svg', 'ps', 'pdf'),
+            help='image format')
+    parser.add_argument('--nframes', type=int, default=100,
+            help='number of animation frames (image files) to create') 
+    parser.add_argument('output_directory',
+            help='path to the output directory for .png frames')
+    main(parser.parse_args())
+
--- TreeProjection.py
+++ TreeProjection.py
@@ -21,6 +21,7 @@ g_xy_scale = 20
 g_z_scale = 20
 g_height_style = 'dashed'
 
+g_cairo_dash_style = [3]
 
 def get_v_to_xyz(yaw, v_to_location, v_to_val):
     """
@@ -59,7 +60,7 @@ def get_radii(v_to_xyz, pitch):
     r_max = max(math.hypot(x,y) for x,y,z in v_to_xyz.values())
     r = r_max * g_radius_inflation
     h_radius = r
-    v_radius = yz_to_y(r, 0, pitch)
+    v_radius = abs(yz_to_y(r, 0, pitch))
     return (h_radius, v_radius)
 
 def v_to_shadow(v):
@@ -106,6 +107,7 @@ def xyz_to_tikz_lines(T, B, pitch, v_to_xyz,
     @param pitch: an angle; worm eye vs bird eye view of the tree
     @param v_to_xyz: maps vertices to location and valuations
     """
+    direction  = math.copysign(1, pitch)
     tikz_lines = []
     plain_vertices = leaves + internal
     # draw shadow vertices
@@ -127,7 +129,7 @@ def xyz_to_tikz_lines(T, B, pitch, v_to_xyz,
     # draw the non-positively valuated vertices
     for v in plain_vertices:
         x, y, z = v_to_xyz[v]
-        if z >= 0:
+        if z*direction >= 0:
             continue
         if v in leaves:
             style = 'draw,shape=circle,fill=black,minimum size=3pt'
@@ -139,15 +141,14 @@ def xyz_to_tikz_lines(T, B, pitch, v_to_xyz,
     # draw the edge segments that have non-positive valuation
     for u_edge in T:
         a, b = u_edge
-        eps = 1e-8
-        if max(v_to_xyz[a][-1], v_to_xyz[b][-1]) >= eps:
+        if avg(v_to_xyz[a][-1], v_to_xyz[b][-1])*direction >= 0:
             continue
         line = '\\path (%s) edge node {} (%s);' % (a, b)
         tikz_lines.append(line)
     # draw the height bars to non-positively valuated vertices
     for v in plain_vertices:
         x, y, z = v_to_xyz[v]
-        if z >= 0:
+        if z*direction >= 0:
             continue
         line = '\\path[%s] (%s) edge node {} (%s);' % (
                 g_height_style, v, v_to_shadow(v))
@@ -161,7 +162,7 @@ def xyz_to_tikz_lines(T, B, pitch, v_to_xyz,
     # draw the positively valuated vertices
     for v in plain_vertices:
         x, y, z = v_to_xyz[v]
-        if z < 0:
+        if z*direction < 0:
             continue
         if v in leaves:
             style = 'draw,shape=circle,fill=black,minimum size=3pt'
@@ -173,15 +174,14 @@ def xyz_to_tikz_lines(T, B, pitch, v_to_xyz,
     # draw the edge segments that have positive valuation
     for u_edge in T:
         a, b = u_edge
-        eps = 1e-8
-        if max(v_to_xyz[a][-1], v_to_xyz[b][-1]) < eps:
+        if avg(v_to_xyz[a][-1], v_to_xyz[b][-1])*direction < 0:
             continue
         line = '\\path (%s) edge node {} (%s);' % (a, b)
         tikz_lines.append(line)
     # draw the height bars to positively valuated vertices
     for v in plain_vertices:
         x, y, z = v_to_xyz[v]
-        if z < 0:
+        if z*direction < 0:
             continue
         line = '\\path[%s] (%s) edge node {} (%s);' % (
                 g_height_style, v, v_to_shadow(v))
@@ -222,6 +222,149 @@ def get_tikz_lines(newick, eigenvector_index, yaw, pitch):
     intersection_vertices = sorted(v for v in v_to_xyz if v not in vertices)
     # get lines of the tikz file
     return xyz_to_tikz_lines(T, B, pitch, v_to_xyz,
+            leaves, internal, intersection_vertices)
+
+def avg(a, b):
+    return 0.5*(a+b)
+
+def xyz_to_cairo(ctx, T, B, pitch, v_to_xyz,
+        leaves, internal, intersection_vertices):
+    """
+    The x and y locations are centered.
+    The z locations are the harmonically extended valuations.
+    @param scale: more scaling
+    @param ctx: cairo context
+    @param T: tree topology
+    @param B: branch lengths
+    @param pitch: an angle; worm eye vs bird eye view of the tree
+    @param v_to_xyz: maps vertices to location and valuations
+    """
+    direction  = math.copysign(1, pitch)
+    tikz_lines = []
+    plain_vertices = leaves + internal
+    # draw the non-positively valuated vertices
+    for v in leaves:
+        x, y, z = v_to_xyz[v]
+        if z*direction >= 0:
+            continue
+        ctx.save()
+        ctx.arc(x, yz_to_y(y, z, pitch), 3, 0, 2*math.pi)
+        ctx.fill()
+        ctx.restore()
+    # draw the edge segments that have non-positive valuation
+    for u_edge in T:
+        a, b = u_edge
+        if avg(v_to_xyz[a][-1], v_to_xyz[b][-1])*direction >= 0:
+            continue
+        ax = v_to_xyz[a][0]
+        ay = yz_to_y(v_to_xyz[a][1], v_to_xyz[a][2], pitch)
+        bx = v_to_xyz[b][0]
+        by = yz_to_y(v_to_xyz[b][1], v_to_xyz[b][2], pitch)
+        ctx.save()
+        ctx.move_to(ax, ay)
+        ctx.line_to(bx, by)
+        ctx.stroke()
+        ctx.restore()
+    # draw the height bars to non-positively valuated vertices
+    for v in plain_vertices:
+        x, y, z = v_to_xyz[v]
+        if z*direction >= 0:
+            continue
+        ax = v_to_xyz[v][0]
+        ay = yz_to_y(v_to_xyz[v][1], v_to_xyz[v][2], pitch)
+        bx = v_to_xyz[v][0]
+        by = yz_to_y(v_to_xyz[v][1], 0, pitch)
+        ctx.save()
+        ctx.set_dash(g_cairo_dash_style)
+        ctx.move_to(ax, ay)
+        ctx.line_to(bx, by)
+        ctx.stroke()
+        ctx.restore()
+    # draw the translucent ellipse
+    h_radius, v_radius = get_radii(v_to_xyz, pitch)
+    eps = 1e-8
+    if v_radius > eps:
+        ctx.save()
+        ctx.scale(h_radius, v_radius)
+        ctx.arc(0, 0, 1, 0, 2*math.pi)
+        ctx.set_source_rgba(0.6, 0.6, 0.6, 0.8)
+        ctx.fill()
+        ctx.restore()
+    # draw the positively valuated vertices
+    for v in leaves:
+        x, y, z = v_to_xyz[v]
+        if z*direction < 0:
+            continue
+        ctx.save()
+        ctx.arc(x, yz_to_y(y, z, pitch), 3, 0, 2*math.pi)
+        ctx.fill()
+        ctx.restore()
+    # draw the edge segments that have positive valuation
+    for u_edge in T:
+        a, b = u_edge
+        if avg(v_to_xyz[a][-1], v_to_xyz[b][-1])*direction < 0:
+            continue
+        ax = v_to_xyz[a][0]
+        ay = yz_to_y(v_to_xyz[a][1], v_to_xyz[a][2], pitch)
+        bx = v_to_xyz[b][0]
+        by = yz_to_y(v_to_xyz[b][1], v_to_xyz[b][2], pitch)
+        ctx.save()
+        ctx.move_to(ax, ay)
+        ctx.line_to(bx, by)
+        ctx.stroke()
+        ctx.restore()
+    # draw the height bars to positively valuated vertices
+    for v in plain_vertices:
+        x, y, z = v_to_xyz[v]
+        if z*direction < 0:
+            continue
+        ax = v_to_xyz[v][0]
+        ay = yz_to_y(v_to_xyz[v][1], v_to_xyz[v][2], pitch)
+        bx = v_to_xyz[v][0]
+        by = yz_to_y(v_to_xyz[v][1], 0, pitch)
+        ctx.save()
+        ctx.set_dash(g_cairo_dash_style)
+        ctx.move_to(ax, ay)
+        ctx.line_to(bx, by)
+        ctx.stroke()
+        ctx.restore()
+
+def draw_cairo_frame(ctx, scale, newick, eigenvector_index, yaw, pitch):
+    """
+    @param eigenvector_index: 1 is Fiedler
+    """
+    tree = Newick.parse(newick, SpatialTree.SpatialTree) 
+    # change the node names and get the new tree string
+    for node in tree.preorder():
+        node.name = 'n' + str(id(node))
+    newick = NewickIO.get_newick_string(tree)
+    # do the layout
+    layout = FastDaylightLayout.StraightBranchLayout() 
+    layout.do_layout(tree) 
+    tree.fit((g_xy_scale, g_xy_scale))
+    name_to_location = dict((
+        x.name, tree._layout_to_display(x.location)) for x in tree.preorder())
+    T, B, N = FtreeIO.newick_to_TBN(newick)
+    # get some vertices
+    leaves = Ftree.T_to_leaves(T)
+    internal = Ftree.T_to_internal_vertices(T)
+    vertices = leaves + internal
+    # get the locations
+    v_to_location = dict((v, name_to_location[N[v]]) for v in vertices)
+    # get the valuations
+    w, V = Ftree.TB_to_harmonic_extension(T, B, leaves, internal)
+    index_to_val = V[:, eigenvector_index-1]
+    v_to_val = dict(
+            (vertices[i], g_z_scale*val) for i, val in enumerate(index_to_val))
+    # get the coordinates
+    v_to_xyz = get_v_to_xyz(yaw, v_to_location, v_to_val)
+    # add intersection vertices
+    add_intersection_vertices(T, B, v_to_xyz)
+    intersection_vertices = sorted(v for v in v_to_xyz if v not in vertices)
+    # FIXME everything above is the same as in get_tikz_lines
+    for v in v_to_xyz:
+        v_to_xyz[v] *= scale
+    xyz_to_cairo(ctx, T, B, pitch, v_to_xyz,
             leaves, internal, intersection_vertices)
 
 if __name__ == '__main__':
