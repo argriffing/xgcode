1073fb99cf617651ccc1c325462390ec476ca31b
a g <argriffi@brcdesk18.statgen.ncsu.edu>
2012-03-26 18:48:36 -0400

    remove some dead code

--- Newick.py
+++ Newick.py
@@ -324,8 +324,6 @@ def main():
 
 if __name__ == '__main__':
     parser = OptionParser()
-    #parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False)
-    #parser.add_option('-o', '--output', dest='output_filename', metavar='FILE', help='output file')
     parser.add_option('--test', action='store_true', dest='test', default=False)
     options, args = parser.parse_args()
     if options.test:
