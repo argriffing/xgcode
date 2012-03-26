f41cf97fddcb159077b6685264122e85fb41e469
a g <argriffi@brcdesk18.statgen.ncsu.edu>
2012-03-26 13:35:35 -0400

    add an unused module

---
+++ asg.py
@@ -0,0 +1,16 @@
+"""
+Ancestral selection graph module.
+Stephen Krone and Claudia Neuhauser,
+Ancestral Processes with Selection, 1997.
+"""
+
+import unittest
+
+
+
+class TestMe(unittest.TestCase):
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
