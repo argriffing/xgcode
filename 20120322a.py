712ecf3c2af6fd9312c7219658dd245f81637e9a
a g <argriffi@brcdesk18.statgen.ncsu.edu>
2012-03-26 10:21:42 -0400

    fix the date in the filenames for the spectral rate snippets

---
+++ 20120322a.py
@@ -0,0 +1,82 @@
+"""
+Look at relaxation times of aggregated independent site reversible CTMC models.
+
+Two independently evolving sites are aggregated.
+"""
+
+from StringIO import StringIO
+import math
+import itertools
+import random
+
+import numpy as np
+import scipy
+from scipy import linalg
+
+import Form
+import FormOut
+from MatrixUtil import ndot
+import mrate
+import divtime
+
+def get_form():
+    """
+    @return: the body of a form
+    """
+    # define the form objects
+    form_objects = [
+            Form.Integer('nstates', 'use this many states per site',
+                3, low=2, high=4)]
+    return form_objects
+
+def get_form_out():
+    return FormOut.Report()
+
+def aggregate(R):
+    n = len(R)
+    Q = np.zeros((n*n, n*n))
+    for a in range(n):
+        for b in range(n):
+            for na in range(n):
+                if na != a:
+                    Q[a * n + b, na * n + b] = R[a, na]
+            for nb in range(n):
+                if nb != b:
+                    Q[a * n + b, a * n + nb] = R[b, nb]
+    Q -= np.diag(np.sum(Q, axis=1))
+    return Q
+
+def get_response_content(fs):
+    out = StringIO()
+    np.set_printoptions(linewidth=200)
+    # get the user defined variables
+    n = fs.nstates
+    # sample a random reversible CTMC rate matrix
+    v = divtime.sample_distribution(n)
+    S = divtime.sample_symmetric_rate_matrix(n)
+    R = divtime.to_gtr_halpern_bruno(S, v)
+    distn = mrate.R_to_distn(R)
+    spectrum = scipy.linalg.eigvalsh(mrate.symmetrized(R))
+    print >> out, 'random reversible CTMC rate matrix:'
+    print >> out, R
+    print >> out
+    print >> out, 'stationary distribution:'
+    print >> out, distn
+    print >> out
+    print >> out, 'spectrum:'
+    print >> out, spectrum
+    print >> out
+    Q = aggregate(R)
+    distn = mrate.R_to_distn(Q)
+    spectrum = scipy.linalg.eigvalsh(mrate.symmetrized(Q))
+    print >> out, 'aggregated rate matrix:'
+    print >> out, Q
+    print >> out
+    print >> out, 'stationary distribution:'
+    print >> out, distn
+    print >> out
+    print >> out, 'spectrum:'
+    print >> out, spectrum
+    print >> out
+    return out.getvalue()
+
---
+++ 20120322b.py
@@ -0,0 +1,118 @@
+"""
+Use tikz to draw a sample path from a continuous time Markov chain.
+"""
+
+from StringIO import StringIO
+
+import random
+import numpy as np
+import scipy
+from scipy import linalg
+
+import Form
+import FormOut
+import tikz
+import latexutil
+import iterutils
+import mrate
+
+g_default_matrix_string = """\
+-2   2  0
+49 -98 49
+ 0   2 -2
+"""
+
+g_default_matrix = np.array([
+    [-2, 2, 0],
+    [49, -98, 49],
+    [0, 2, -2]])
+
+g_default_colors = ['blue', 'red', 'yellow']
+
+
+def get_form():
+    """
+    @return: the body of a form
+    """
+    # define the form objects
+    form_objects = [
+            Form.Matrix('R', 'rate matrix',
+                g_default_matrix),
+            Form.Sequence('colorseq', 'user-defined colors',
+                g_default_colors),
+            Form.Float('interval', 'amount of elapsed time',
+                '1.0', low_exclusive=0, high_exclusive=100),
+            Form.Float('tikzlength', 'tikz line length',
+                '6.0', low_exclusive=1, high_exclusive=200),
+            Form.TikzFormat(),
+            Form.ContentDisposition()]
+    return form_objects
+
+def get_form_out():
+    return FormOut.Tikz()
+
+def sample_from_weights(weights):
+    """
+    Sample from a finite distribution defined by nonnegative weights.
+    """
+    probs = np.array(weights, dtype=float) / sum(weights)
+    u = random.random()
+    total = 0
+    for i, p in enumerate(probs):
+        total += p
+        if u <= total:
+            return i
+
+def get_draw_line(c, x1, y1, x2, y2):
+    return '\\draw[color=%s,very thick] (%0.4f,%0.4f) -- (%0.4f,%0.4f);' % (
+            c, x1, y1, x2, y2)
+
+def get_tikz_lines(fs):
+    # read the user variables
+    R = np.array(fs.R)
+    colors = fs.colorseq
+    t_max = fs.interval
+    length_total = fs.tikzlength
+    # sample the initial state
+    nstates = len(R)
+    distn = mrate.R_to_distn(R)
+    states = [sample_from_weights(distn)]
+    times = [0.0]
+    # sample the path
+    while True:
+        cur_state = states[-1]
+        rate = -R[cur_state, cur_state]
+        t = random.expovariate(rate)
+        t_next = times[-1] + t
+        if t_next >= t_max:
+            break
+        else:
+            times.append(t_next)
+            weights = np.maximum(R[cur_state], 0)
+            states.append(sample_from_weights(weights))
+    states.append(states[-1])
+    times.append(t_max)
+    # draw the path
+    nsegs = len(states) - 1
+    out = StringIO()
+    lines = []
+    for i in range(nsegs):
+        state_a, state_b = states[i:i+2]
+        times_a, times_b = times[i:i+2]
+        xa = ((times_a / t_max) - 0.5) * length_total
+        xb = ((times_b / t_max) - 0.5) * length_total
+        y = 0
+        line = get_draw_line(colors[state_a], xa, y, xb, y)
+        lines.append(line)
+    return lines
+
+
+def get_response_content(fs):
+    """
+    @param fs: a FieldStorage object containing the cgi arguments
+    @return: the response
+    """
+    tikz_body = '\n'.join(get_tikz_lines(fs))
+    tikzpicture = tikz.get_picture(tikz_body, 'auto')
+    return tikz.get_response(tikzpicture, fs.tikzformat)
+
