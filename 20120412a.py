c4a1e0b97600dc3aa45ffc21cc46b996e8992b21
a g <argriffi@brcdesk18.statgen.ncsu.edu>
2012-04-12 21:27:05 -0400

    add a beast analysis that uses loganalyser

---
+++ 20120412a.py
@@ -0,0 +1,59 @@
+"""
+Use BEAST to analyze an interval of the primate tutorial alignment. [UNFINISHED]
+
+Web service output is the loganalyser output.
+The extended command line functionality
+will eventually include
+the analysis of multiple intervals
+using a separate BEAST instance for each interval.
+"""
+
+from StringIO import StringIO
+import argparse
+import multiprocessing
+
+import Form
+import FormOut
+import beasttut
+import beast
+
+def get_form():
+    """
+    @return: the body of a form
+    """
+    form_objects = [
+            Form.Integer('start',
+                'sub-sequence start position (1-%d)' % beasttut.g_nchar,
+                1, low=1, high=beasttut.g_nchar),
+            Form.Integer('stop',
+                'sub-sequence stop position (1-%d)' % beasttut.g_nchar,
+                beasttut.g_nchar, low=1, high=beasttut.g_nchar),
+            Form.Integer('nsamples', 'mcmc chain steps',
+                8000, low=80, high=8000)]
+    return form_objects
+
+def get_form_out():
+    return FormOut.Report('summary.txt')
+
+def get_response_content(fs):
+    # init the response and get the user variables
+    start_pos = fs.start
+    stop_pos = fs.stop
+    nsamples = fs.nsamples
+    out = StringIO()
+    # get the xml contents
+    xml_string = beasttut.get_xml_string(
+            start_pos, stop_pos, nsamples, 'myjob.log',
+            beasttut.get_header_seq_pairs())
+    # prepare the base path for the beast analysis
+    basepath = beast.prepare()
+    with open(os.path.join(basepath, 'myjob.xml'), 'w') as fout:
+        fout.write(xml_string + '\n')
+    beast.run_beast(basepath)
+    beast.run_loganalyser(basepath)
+    # return the analysis
+    with open(os.path.join(basepath, 'myjob-loganalyser.txt')) as fin:
+        analysis_text = fin.read()
+    # return the response
+    return analysis_text
+
--- beast.py
+++ beast.py
@@ -1,193 +1,74 @@
 """
-A module for interfacing with BEAST.
+A module for interfacing with the BEAST MCMC java program.
+
+This assumes that BEAST is on the path and that BEAGLE has been installed.
+1) prepare() the base path
+2) write the "<basepath>/myjob.xml" file referencing "myjob.log"
+3) run_beast(basepath)
+4) run_loganalyser(basepath)
 """
 
-# FIXME UNUSED
-
-from StringIO import StringIO
-import os
 import subprocess
-import argparse
-import logging
-import multiprocessing
+import time
+import os
 
-import Form
-import FormOut
-import const
 import Util
-import RUtil
-import hpcutil
-import beasttut
 
 
-g_remote_beast_sh_path = os.path.join(
-        '/brc_share/brc/argriffi/packages/BEASTv1.7.1',
-        'bin/beast')
-g_remote_beast_jar_path = os.path.join(
-        '/brc_share/brc/argriffi/packages/BEASTv1.7.1',
-        'lib/beast.jar')
-g_remote_java_path = '/usr/local/apps/java/jre1.6.0_31/bin/java'
-
-
-
-class RemoteBeast(hpcutil.RemoteBrc):
-    def __init__(self, start_stop_pairs, nsamples):
-        hpcutil.RemoteBrc.__init__(self)
-        self.start_stop_pairs = start_stop_pairs
-        self.nsamples = nsamples
-        self.local_log_paths = None
-    def preprocess(self):
-        # for each (start_pos, stop_pos) pair
-        # define a log filename and
-        # create a beast xml and a bsub script
-        self.local_log_paths = []
-        for i, start_stop_pair in enumerate(self.start_stop_pairs):
-            start_pos, stop_pos = start_stop_pair
-            # define the log file paths
-            log_name = 'beast-%d-%d.log' % start_stop_pair
-            local_log_path = os.path.join(
-                    self.local.get_out(), log_name)
-            remote_log_path = os.path.join(
-                    self.remote.get_out(), log_name)
-            self.local_log_paths.append(local_log_path)
-            # define the xml file paths
-            xml_name = 'beast-%d-%d.xml' % start_stop_pair
-            local_xml_path = os.path.join(
-                    self.local.get_in_contents(), xml_name)
-            remote_xml_path = os.path.join(
-                    self.remote.get_in_contents(), xml_name)
-            # define the local bsub path
-            bsub_name = 'beast-%d-%d.bsub' % start_stop_pair
-            local_bsub_path = os.path.join(
-                    self.local.get_in_bsubs(), bsub_name)
-            # create the xml file
-            with open(local_xml_path, 'w') as fout:
-                print >> fout, beasttut.get_xml_string(
-                        start_pos, stop_pos, self.nsamples, remote_log_path)
-            # create the bsub file
-            with open(local_bsub_path, 'w') as fout:
-                stdout_path = os.path.join(self.remote.get_out(),
-                        'out.beast-%d-%d' % start_stop_pair)
-                stderr_path = os.path.join(self.remote.get_out(),
-                        'err.beast-%d-%d' % start_stop_pair)
-                # name the job
-                print >> fout, '#BSUB -J %d-%d' % start_stop_pair
-                # suggest the brc queue
-                print >> fout, '#BSUB -q brc'
-                # redirect stdout
-                print >> fout, '#BSUB -o', stdout_path
-                # redirect stderr
-                print >> fout, '#BSUB -e', stderr_path
-                # run BEAST using a java path suggested by Gary Howell
-                print >> fout, g_remote_java_path, '-jar'
-                print >> fout, g_remote_beast_jar_path, remote_xml_path
-
-def get_table_string_and_scripts(start_stop_pairs, nsamples):
+def prepare():
     """
-    Command-line only.
+    Create the job directory.
+    @return: path to a newly created job directory
     """
-    # build the array for the R table
-    data_arr = []
-    sequence_lengths = []
-    midpoints = []
-    for start_pos, stop_pos in start_stop_pairs:
-        sequence_length = stop_pos - start_pos + 1
-        means, variations, covs = get_value_lists(
-                start_pos, stop_pos, nsamples)
-        midpoint = (start_pos + stop_pos) / 2.0
-        row = [sequence_length, midpoint]
-        for values in means, variations, covs:
-            corr_info = mcmc.Correlation()
-            corr_info.analyze(values)
-            hpd_low, hpd_high = mcmc.get_hpd_interval(0.95, values)
-            row.extend([hpd_low, corr_info.mean, hpd_high])
-        data_arr.append(row)
-        sequence_lengths.append(sequence_length)
-        midpoints.append(midpoint)
-    # build the table string
-    table_string = RUtil.get_table_string(data_arr, g_headers)
-    # get the scripts
-    scripts = get_ggplot2_scripts(nsamples, sequence_lengths, midpoints)
-    # return the table string and scripts
-    return table_string, scripts
+    # reserve a name
+    data = 'reserving a unique random local name...\n'
+    tmp_path = Util.create_tmp_file(data=data, prefix='', suffix='')
+    tmp_name = os.path.basename(tmp_path)
+    t = time.gmtime()
+    timestr = '%04d%02d%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
+    newdir = '_'.join(('beast', timestr, tmpname))
+    # create the input and output directories
+    os.makedirs(newdir)
+    # return the base directory
+    return newdir
 
-def forked_function(start_stop_n):
+def run_beast(base_path):
     """
-    This function should accept and return as little data as possible.
-    In particular do not return a huge multimegabyte nested list.
-    @param start_stop_n: start_pos, stop_pos, nsamples
-    @return: (corr_info, hpd_interval) for mean, variation, covariance
+    The base path will be used as the working directory.
+    Whatever miscellaneous intermediate files beast wants to make
+    will be created in that directory.
+    The base path is assumed to contain the xml named myjob.xml.
+    The log file named myjob.log will be created in that directory.
+    Also myjob-beast.out and myjob-beast.err.
+    @param base_path: base path to run this job
+    @return: None
     """
-    start_pos, stop_pos, nsamples = start_stop_n
-    means, variations, covs = get_value_lists(start_pos, stop_pos, nsamples)
-    post_pairs = []
-    for values in means, variations, covs:
-        corr_info = mcmc.Correlation()
-        corr_info.analyze(values)
-        hpd_interval = mcmc.get_hpd_interval(0.95, values)
-        post_pairs.append((corr_info, hpd_interval))
-    return post_pairs
+    out_path = os.path.join(base_path, 'myjob-beast.out')
+    err_path = os.path.join(base_path, 'myjob-beast.err')
+    args = [
+            'beast',
+            '-beagle', '-beagle_CPU', '-beagle_SSH', '-beast_double',
+            '-working',
+            'myjob.xml']
+    with open(out_path, 'w') as fout:
+        with open(err_path, 'w') as ferr:
+            p = subprocess.Popen(args, stdout=fout, stderr=ferr)
+            p.communicate()
 
-def get_table_string_and_scripts_par(start_stop_pairs, nsamples):
+def run_loganalyser(base_path):
     """
-    Local command-line multi-process only.
+    @param base_path: base path used in beast analysis
+    @return: None
     """
-    # define the pool of processes corresponding to the number of cores
-    mypool = Pool(processes=4)
-    # do the multiprocessing
-    start_stop_n_triples = [(a, b, nsamples) for a, b in start_stop_pairs]
-    post_pairs_list = mypool.map(forked_function, start_stop_n_triples)
-    # build the array for the R table
-    data_arr = []
-    sequence_lengths = []
-    midpoints = []
-    for start_stop_pair, post_pairs in zip(start_stop_pairs, post_pairs_list):
-        start_pos, stop_pos = start_stop_pair
-        sequence_length = stop_pos - start_pos + 1
-        midpoint = (start_pos + stop_pos) / 2.0
-        row = [sequence_length, midpoint]
-        for corr_info, hpd_interval in post_pairs:
-            hpd_low, hpd_high = hpd_interval
-            row.extend([hpd_low, corr_info.mean, hpd_high])
-        data_arr.append(row)
-        sequence_lengths.append(sequence_length)
-        midpoints.append(midpoint)
-    # build the table string
-    table_string = RUtil.get_table_string(data_arr, g_headers)
-    # get the scripts
-    scripts = get_ggplot2_scripts(nsamples, sequence_lengths, midpoints)
-    # return the table string and scripts
-    return table_string, scripts
-
-def get_table_string_and_scripts_from_logs(
-        start_stop_pairs, log_paths, nsamples):
-    """
-    This is for analysis of remote execution.
-    """
-    # build the array for the R table
-    data_arr = []
-    sequence_lengths = []
-    midpoints = []
-    for start_stop_pair, log_path in zip(
-            start_stop_pairs, log_paths):
-        start_pos, stop_pos = start_stop_pair
-        sequence_length = stop_pos - start_pos + 1
-        means, variations, covs = read_log(log_path, nsamples)
-        midpoint = (start_pos + stop_pos) / 2.0
-        row = [sequence_length, midpoint]
-        for values in means, variations, covs:
-            corr_info = mcmc.Correlation()
-            corr_info.analyze(values)
-            hpd_low, hpd_high = mcmc.get_hpd_interval(0.95, values)
-            row.extend([hpd_low, corr_info.mean, hpd_high])
-        data_arr.append(row)
-        sequence_lengths.append(sequence_length)
-        midpoints.append(midpoint)
-    # build the table string
-    table_string = RUtil.get_table_string(data_arr, g_headers)
-    # get the scripts
-    scripts = get_ggplot2_scripts(nsamples, sequence_lengths, midpoints)
-    # return the table string and scripts
-    return table_string, scripts
-
+    log_path = os.path.join(base_path, 'myjob.log')
+    txt_path = os.path.join(base_path, 'myjob-loganalyser.txt')
+    out_path = os.path.join(base_path, 'myjob-loganalyser.out')
+    err_path = os.path.join(base_path, 'myjob-loganalyser.err')
+    args = [
+            'loganalyser',
+            '-hpd', '-ess', log_path, txt_path]
+    with open(out_path, 'w') as fout:
+        with open(err_path, 'w') as ferr:
+            p = subprocess.Popen(args, cwd=base_path, stdout=fout, stderr=ferr)
+            p.communicate()
 
