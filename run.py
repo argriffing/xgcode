2f0d75fbce46b92fb28e4bda9eeca029b9ef65c9
a g <argriffi@brcdesk18.statgen.ncsu.edu>
2012-04-23 09:37:45 -0400

    add more documentation to the web script framework launcher'

--- run.py
+++ run.py
@@ -274,8 +274,10 @@ def create_documentation():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default='127.0.0.1')
-    parser.add_argument('--port', type=int, default=8080)
+    parser.add_argument('--host', type=str, default='127.0.0.1',
+            help='use 0.0.0.0 for all public ports')
+    parser.add_argument('--port', type=int, default=8080,
+            help='should be at least 1024 unless you have root access')
     parser.add_argument('--mkdocs', action='store_true', help='build docs')
     args = parser.parse_args()
     if g_current_directory == g_script_directory:
