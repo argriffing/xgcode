9309db1e1be4c1c3d170deb388e82172b9107e5a
a g <argriffi@brcdesk18.statgen.ncsu.edu>
2012-06-02 18:36:53 -0400

    add a script to find a second critical divergence time for 3-state models

--- 20120602b.py
+++ 20120602b.py
@@ -1,12 +1,13 @@
 r"""
-Seek the first critical divtime for Fisher info for a 3-state process.
+Seek the second critical divtime for Fisher info for a 3-state process.
 
 The mutation process is assumed to be site-independent
 with three sites and two states per site.
-At short times, the process without selection
-gives more Fisher information.
-At medium times, the process with a cycle-like lethality pattern
-give more information.
+At medium times, a process with a cycle-like lethality pattern
+gives more information.
+At long times, a non-lethal selection process
+parameterized by a single degree of freedom
+gives more information.
 """
 
 from StringIO import StringIO
@@ -27,24 +28,34 @@ def get_form():
     """
     return [
             Form.Float('a', 'a divtime bracket endpoint',
-                '0.65', low_inclusive=0),
+                '1.13', low_inclusive=0),
             Form.Float('b', 'a divtime bracket endpoint',
-                '0.70', low_inclusive=0),
+                '1.15', low_inclusive=0),
             ]
 
 def get_form_out():
     return FormOut.Report('results')
 
-class Opt:
+class OptMin:
+    """
+    This is for minimization.
+    """
+    def __init__(self, t):
+        self.t = t
+        d = 3
+        self.obj = evozoo.AlternatingHypercube_d_1(d)
+    def __call__(self, X):
+        Q = self.obj.get_rate_matrix(X)
+        distn = self.obj.get_distn(X)
+        info = divtime.get_fisher_info_known_distn_fast(Q, distn, self.t)
+        return -info
+
+class OptRoot:
     """
     This is for brentq root finding.
     """
     def __init__(self):
         d = 3
-        # precompute the hypercube
-        zoo_obj = evozoo.Hypercube_d_0(d)
-        self.hypercube_Q = zoo_obj.get_rate_matrix()
-        self.hypercube_distn = zoo_obj.get_distn()
         # precompute the coil
         zoo_obj = evozoo.Coil_d_0(d)
         self.coil_Q = zoo_obj.get_rate_matrix()
@@ -52,20 +63,25 @@ class Opt:
     def __call__(self, t):
         """
         @param t: divergence time
-        @return: signed difference between target and mutual information
+        @return: signed difference between infos
         """
-        hypercube_info = divtime.get_fisher_info_known_distn_fast(
-                self.hypercube_Q, self.hypercube_distn, t)
+        # get the max parameterized cube info
+        opt_min = OptMin(t)
+        X0 = np.random.randn(1)
+        xopt = scipy.optimize.fmin(opt_min, X0)
+        max_cube_info = -opt_min(xopt)
+        # get the coil info
         coil_info = divtime.get_fisher_info_known_distn_fast(
                 self.coil_Q, self.coil_distn, t)
-        return coil_info - hypercube_info
+        # return the info difference
+        return max_cube_info - coil_info
 
 def get_response_content(fs):
-    opt = Opt()
+    opt = OptRoot()
     if opt(fs.a) * opt(fs.b) >= 0:
         raise ValueError(
                 'please choose a bracket that more obviously '
-                'contains the right entropy')
+                'contains a root')
     t_opt = scipy.optimize.brentq(opt, fs.a, fs.b)
     return str(t_opt)
 
