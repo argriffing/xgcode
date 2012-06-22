5e9e932eed79c2cd0e159619534915552eb7f327
a g <argriffi@brcdesk18.(none)>
2012-06-22 17:03:32 -0400

    add a no-recombination transition matrix creation function for the reduced state space and add a couple of corresponding unit tests

--- pgmfancy.py
+++ pgmfancy.py
@@ -14,7 +14,6 @@ but less efficient longer state space.
 
 import unittest
 from itertools import product
-from collections import defaultdict
 
 import numpy as np
 from numpy import linalg
@@ -100,6 +99,33 @@ def get_selection_recombination_transition_matrix_s(
             P[parent_short, child_short] = short_to_count[child_short] * p
     return P
 
+def get_selection_transition_matrix_s(
+        ci_to_short, short_to_count, sorted_chrom_lists,
+        selection, nchromosomes, npositions):
+    """
+    Note that this includes only selection and not recombination or mutation.
+    Therefore the transition matrix will be very sparse.
+    @param selection: a fitness ratio
+    @param nchromosomes: number of chromosomes in the population
+    @param npositions: number of positions per chromosome
+    """
+    nstates = len(sorted_chrom_lists)
+    P = np.zeros((nstates, nstates))
+    for parent_short, parent_chroms in enumerate(sorted_chrom_lists):
+        parent_index_distn = np.zeros(nchromosomes)
+        for i, chrom in enumerate(parent_chroms):
+            parent_index_distn[i] = selection**gmpy.popcount(chrom)
+        parent_index_distn /= np.sum(parent_index_distn)
+        for parent_idxs in product(range(nchromosomes), repeat=nchromosomes):
+            child_chroms = [parent_chroms[i] for i in parent_idxs]
+            p = 1
+            for i in parent_idxs:
+                p *= parent_index_distn[i]
+            child_ci = chroms_to_index(sorted(child_chroms), npositions)
+            child_short = ci_to_short[child_ci]
+            P[parent_short, child_short] += p
+    return P
+
 def get_selection_transition_matrix(selection, nchromosomes, npositions):
     """
     Note that this includes only selection and not recombination or mutation.
@@ -108,7 +134,6 @@ def get_selection_transition_matrix(selection, nchromosomes, npositions):
     @param nchromosomes: number of chromosomes in the population
     @param npositions: number of positions per chromosome
     """
-    # init the unnormalized transition matrix
     nstates = 1 << (nchromosomes * npositions)
     P = np.zeros((nstates, nstates))
     for parent_chroms in product(range(1<<npositions), repeat=nchromosomes):
@@ -153,6 +178,17 @@ class TestPGMFancy(unittest.TestCase):
         Pb = popgenmarkov.get_selection_recombination_transition_matrix(
                 selection, recombination, nchromosomes, npositions)
         self.assertTrue(np.allclose(Pa, Pb))
+
+    def test_invariant_selection_transition_s(self):
+        selection = 1.1
+        nchromosomes = 3
+        npositions = 2
+        ci_to_short, short_to_count, sorted_chrom_lists = get_state_space_info(
+                nchromosomes, npositions)
+        P = get_selection_transition_matrix_s(
+                ci_to_short, short_to_count, sorted_chrom_lists,
+                selection, nchromosomes, npositions)
+        MatrixUtil.assert_transition_matrix(P)
 
     def test_invariant_selection_recombination_transition_s(self):
         selection = 1.1
@@ -236,6 +272,72 @@ class TestPGMFancy(unittest.TestCase):
         #
         self.assertTrue(
                 np.allclose(no_mutation_posterior, no_mutation_posterior_s))
+
+    def test_regress_recombination_probability_endpoint_conditioning(self):
+        ngenerations = 10
+        selection = 2.0
+        mutation = 0.0001
+        recombination = 0.001
+        nchromosomes = 2
+        npositions = 4
+        K_initial = np.array([
+            [1,1,1,1],
+            [0,0,0,0]], dtype=np.int8)
+        K_final = np.array([
+            [1,1,0,0],
+            [0,0,1,1]], dtype=np.int8)
+        #
+        nsiteboundaries = npositions - 1
+        ngenboundaries = ngenerations - 1
+        no_recomb_prior = (1 - recombination)**(
+                nsiteboundaries*ngenboundaries*nchromosomes)
+        #
+        initial_long = popgenmarkov.bin2d_to_int(K_initial)
+        final_long = popgenmarkov.bin2d_to_int(K_final)
+        ci_to_short, short_to_count, sorted_chrom_lists = get_state_space_info(
+                nchromosomes, npositions)
+        initial_ci = chroms_to_index(
+                sorted(popgenmarkov.bin_to_int(row) for row in K_initial),
+                npositions)
+        initial_short = ci_to_short[initial_ci]
+        final_ci = chroms_to_index(
+                sorted(popgenmarkov.bin_to_int(row) for row in K_final),
+                npositions)
+        final_short = ci_to_short[final_ci]
+        # get an answer using the less efficient methods
+        P_sr = popgenmarkov.get_selection_recombination_transition_matrix(
+                selection, recombination, nchromosomes, npositions)
+        P_s = get_selection_transition_matrix(
+                selection, nchromosomes, npositions)
+        P_m = popgenmarkov.get_mutation_transition_matrix(
+                mutation, nchromosomes, npositions)
+        p_b_given_a = linalg.matrix_power(
+                np.dot(P_sr, P_m), ngenerations-1)[initial_long, final_long]
+        p_b_given_a_no_recomb = linalg.matrix_power(
+                np.dot(P_s, P_m), ngenerations-1)[initial_long, final_long]
+        no_recomb_posterior = (
+                no_recomb_prior * p_b_given_a_no_recomb) / p_b_given_a
+        # get an answer using the more efficient methods
+        P_sr_s = get_selection_recombination_transition_matrix_s(
+                ci_to_short, short_to_count, sorted_chrom_lists,
+                selection, recombination, nchromosomes, npositions)
+        P_s_s = get_selection_transition_matrix_s(
+                ci_to_short, short_to_count, sorted_chrom_lists,
+                selection, nchromosomes, npositions)
+        P_m_s = get_mutation_transition_matrix_s(
+                ci_to_short, short_to_count, sorted_chrom_lists,
+                mutation, nchromosomes, npositions)
+        p_b_given_a_s = linalg.matrix_power(
+                np.dot(P_sr_s, P_m_s), ngenerations-1)[
+                        initial_short, final_short]
+        p_b_given_a_no_recomb_s = linalg.matrix_power(
+                np.dot(P_s_s, P_m_s), ngenerations-1)[
+                        initial_short, final_short]
+        no_recomb_posterior_s = (
+                no_recomb_prior * p_b_given_a_no_recomb_s) / p_b_given_a_s
+        #
+        self.assertTrue(
+                np.allclose(no_recomb_posterior, no_recomb_posterior_s))
 
 
 if __name__ == '__main__':
