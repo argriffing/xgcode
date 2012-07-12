clean up a bit of code and fix an off-by-one error

StatsUtil.py
@@ -13,11 +13,16 @@ from scipy.special import gammaln
 import iterutils
 
 def binomial_log_pmf(observed_n, max_n, p_success):
-    if not p_success:
+    if p_success == 0.0:
         if observed_n:
             return float('-inf')
         else:
             return 0.0
+    elif p_success == 1.0:
+        if observed_n == max_n:
+            return 0.0
+        else:
+            return float('-inf')
     accum = 0
     accum += gammaln(max_n + 1)
     accum -= gammaln(observed_n + 1)

pgmsinglesite.py
@@ -23,7 +23,7 @@ def create_drift_selection_transition_matrix(npop, selection_ratio):
     P = np.zeros((nstates, nstates))
     for a in range(nstates):
         # compute the i.i.d probability of picking a mutant
-        p = (selection_ratio * a) / (selection_ratio * a + (nstates-a))
+        p = (selection_ratio * a) / (selection_ratio * a + (npop-a))
         for b in range(nstates):
             # These are from a binomial distribution
             # with npop trials and p probability of success per trial.
@@ -31,6 +31,7 @@ def create_drift_selection_transition_matrix(npop, selection_ratio):
             observed_n = b
             max_n = npop
             p_success = p
+            print observed_n, max_n, p_success
             P[a, b] = math.exp(StatsUtil.binomial_log_pmf(
                 observed_n, max_n, p_success))
     return P