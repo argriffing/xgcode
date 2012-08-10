85c5a19f565d1464ba11d73db058f0799368ce39
a g <argriffi@brcdesk18.(none)>
2012-08-09 20:57:01 -0400

    add this module

--- wrightfisher.py
+++ wrightfisher.py
@@ -1,2 +1,13 @@
 """
+Wright-Fisher transitions and their diffusion approximations.
 """
+
+import unittest
+
+class TestWrightFisher(unittest.TestCase):
+    def test_foo(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
