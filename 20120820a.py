add a web script that uses the correct diploid wright fisher selection formula and the right way to sample a few haplotypes from a larger diploid population to reproduce a figure in a paper

20120820a.py
@@ -1,9 +1,9 @@
 """
-Approximate a figure from a publication by Kai Zeng 2010.
+Better approximate a figure from a publication by Kai Zeng 2010.
 
-This approximation uses a fixed population of 5 diploid individuals,
-as opposed to the figure (fig. 2) in the publication which I think
-samples 10 alleles at random from a large population.
+This approximation samples 10 alleles from a
+fixed size population of diploid individuals.
+The figure (fig. 2) in the publication may be for a large population limit.
 """
 
 from StringIO import StringIO
@@ -11,7 +11,6 @@ import time
 import math
 
 import numpy as np
-from scipy import optimize
 
 import Form
 import FormOut
@@ -19,6 +18,7 @@ import MatrixUtil
 import StatsUtil
 import kaizeng
 import wrightfisher
+import Util
 import RUtil
 from RUtil import mk_call_str
 
@@ -27,34 +27,37 @@ def get_form():
     @return: the body of a form
     """
     return [
+            Form.Integer('N_big_diploid', 'diploid population size',
+                100, low=5, high=150),
             Form.ImageFormat(),
             ]
 
 def get_form_out():
     return FormOut.Image('plot')
 
-def get_two_allele_distribution(N, f0, f1):
+def get_two_allele_distribution(N_big, N_small, f0, f1):
     """
     Assumes small genic selection.
     Assumes small mutation.
     The mutational bias does not affect the distribution.
-    @param N: haploid population size
+    @param N_big: total number of alleles in the population
+    @param N_small: number of alleles sampled from the population
     @param f0: fitness of allele 0
     @param f1: fitness of allele 1
     @return: distribution over all non-fixed population states
     """
-    # construct something like a transition matrix
-    nstates = N+1
+    # construct a transition matrix
+    nstates = N_big + 1
     P = np.zeros((nstates, nstates))
     for i in range(nstates):
-        p0, p1 = wrightfisher.genic_diallelic(f0, f1, i, N-i)
+        p0, p1 = wrightfisher.genic_diallelic(f0, f1, i, N_big - i)
         if i == 0:
             P[i, 1] = 1.0
-        elif i == N:
-            P[i, N-1] = 1.0
+        elif i == N_big:
+            P[i, N_big - 1] = 1.0
         else:
             for j in range(nstates):
-                logp = StatsUtil.binomial_log_pmf(j, N, p0)
+                logp = StatsUtil.binomial_log_pmf(j, N_big, p0)
                 P[i, j] = math.exp(logp)
     # find the stationary distribution
     v = MatrixUtil.get_stationary_distribution(P)
@@ -66,54 +69,71 @@ def get_two_allele_distribution(N, f0, f1):
     if not np.allclose(v, np.dot(v, P)):
         raise ValueError
     # return the stationary distribution conditional on dimorphism
-    return v[1:-1] / np.sum(v[1:-1])
+    accum = np.zeros(N_small + 1)
+    denominator = Util.choose(N_big, N_small)
+    for i, p_state in enumerate(v):
+        for j in range(N_small + 1):
+            numerator = Util.choose(i, j) * Util.choose(N_big - i, N_small - j)
+            if not numerator:
+                continue
+            accum[j] += (p_state * numerator) / denominator
+    return accum[1:-1] / np.sum(accum[1:-1])
 
 def get_response_content(fs):
-    N = 10
+    N_small = 10
+    N_big = 2*fs.N_big_diploid
+    if N_big < N_small:
+        raise ValueError('use a larger diploid population size')
+    denominator = Util.choose(N_big, N_small)
     k = 4
     gamma = 1.5
     params_list = [
             (0.008, 1, 1, 0, gamma, 1),
             (0.008, 2, 1, 0, gamma, 1)]
-    allele_histograms = np.zeros((2, N+1))
+    allele_histograms = np.zeros((2, N_small + 1))
     for i, params in enumerate(params_list):
-        mutation, selection = kaizeng.params_to_mutation_fitness(N, params)
-        P = kaizeng.get_transition_matrix(N, k, mutation, selection)
+        mutation, selection = kaizeng.params_to_mutation_fitness(N_big, params)
+        P = kaizeng.get_transition_matrix(N_big, k, mutation, selection)
         v = MatrixUtil.get_stationary_distribution(P)
-        for state_index, counts in enumerate(kaizeng.gen_states(N, k)):
+        for state_index, counts in enumerate(kaizeng.gen_states(N_big, k)):
             if counts[0] and counts[1]:
-                allele_histograms[i, counts[0]] += v[state_index]
+                p_state = v[state_index]
+                for j in range(1, N_small):
+                    numerator = Util.choose(counts[0], j)
+                    if not numerator: continue
+                    numerator *= Util.choose(counts[1], N_small - j)
+                    if not numerator: continue
+                    x = (p_state * numerator) / denominator
+                    allele_histograms[i, j] += x
     # Define the r table.
     # There are nine columns each corresponding to an allele frequency.
     # There are three rows each corresponding to a configuration.
     arr = []
+    # Use the two allele approximation
+    # from mcvean and charlesworth 1999 referred to by zeng 2011.
+    # I'm not sure if I am using the right equation.
+    gamma_0 = 0
+    gamma_1 = 1.5
+    s_0 = -gamma_0 / float(N_big)
+    s_1 = -gamma_1 / float(N_big)
+    hist = np.zeros(N_small+1)
+    for i in range(1, N_small):
+        x = i / float(N_small)
+        hist[i] = math.exp(1*N_big*(s_0 - s_1)*x) / (x*(1-x))
+    h = hist[1:-1]
+    h /= np.sum(h)
+    arr.append(h.tolist())
     # Use the exact two allele distribution.
     # Well, it is exact if I understand the right scaling
     # of the population size and fitnesses.
     f0 = 1.0
-    f1 = 1.0 - gamma / N
+    f1 = 1.0 - gamma / N_big
     #f0 = 1.0 + gamma / N
     #f1 = 1.0
     #f0 = 1.0 + 1.5 / (4*N)
     #f1 = 1.0 - 1.5 / (4*N)
-    h = get_two_allele_distribution(N, f0, f1)
+    h = get_two_allele_distribution(N_big, N_small, f0, f1)
     arr.append(h.tolist())
-    # Use the two allele approximation
-    # from mcvean and charlesworth 1999 referred to by zeng 2011.
-    # I'm not sure if I am using the right equation.
-    """
-    gamma_0 = 0
-    gamma_1 = 1.5
-    s_0 = -gamma_0 / float(N)
-    s_1 = -gamma_1 / float(N)
-    hist = np.zeros(N+1)
-    for i in range(1, N):
-        x = i / float(N)
-        hist[i] = math.exp(1*N*(s_0 - s_1)*x) / (x*(1-x))
-    h = hist[1:-1]
-    h /= np.sum(h)
-    arr.append(h.tolist())
-    """
     # Get frequencies for the other two configurations
     for hist in allele_histograms:
         h = hist[1:-1]
@@ -128,6 +148,7 @@ def get_response_content(fs):
             'mdat',
             'legend.text=' + mk_call_str(
                 'c',
+                '"two-allele diffusion limit"',
                 '"two-allele"',
                 '"four-allele without mutational bias"',
                 '"four-allele with mutational bias kappa_{1,2}=2"',
@@ -139,7 +160,7 @@ def get_response_content(fs):
             ylab='"frequency"',
             col=mk_call_str(
                 'c',
-                #'"red"',
+                '"red"',
                 '"white"',
                 '"black"',
                 '"gray"',