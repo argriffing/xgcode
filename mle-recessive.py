37e937aafa8e95c02dccae68fdb324f62ea705e0
a g <argriffi@brcdesk18.(none)>
2012-10-05 01:55:17 -0400

    the maximum likelihood script now runs but gets a different value than the published one

--- mle-recessive.py
+++ mle-recessive.py
@@ -8,6 +8,7 @@ because for some reason I am adopting a more matlab-like style.
 All matrices in this script are ndarrays as opposed to actual python matrices.
 """
 
+import math
 import argparse
 from itertools import product
 
@@ -45,6 +46,9 @@ g_code = {
         ('gtt', 'gtc', 'gta', 'gtg'),
         ('taa', 'tga', 'tag'),
         }
+
+g_mtdna_names = {'human_horai', 'chimp_horai'}
+
 
 def get_fixation_genic(x):
     """
@@ -116,6 +120,7 @@ def get_compo(codons):
     Speed is not important.
     @return: integer matrix
     """
+    ncodons = len(codons)
     compo = np.zeros((ncodons, 4), dtype=int)
     for i, c in enumerate(codons):
         for j, nt in enumerate('acgt'):
@@ -135,6 +140,7 @@ def get_asym_compo(codons):
     Speed is not important.
     @return: a three dimensional matrix
     """
+    ncodons = len(codons)
     asym_compo = np.zeros((ncodons, ncodons, 4), dtype=int)
     for i, ci in enumerate(codons):
         for j, cj in enumerate(codons):
@@ -169,48 +175,183 @@ def get_selection_S(F):
     e = np.ones_like(F)
     return np.outer(e, F) - np.outer(F, e)
 
-def get_fixation(h, S):
+def get_P(
+        ts, tv, syn, nonsyn, compo, asym_compo,
+        h,
+        log_counts,
+        log_mu, log_kappa, log_omega, log_nt_weights):
     """
     Notation is from Yang and Nielsen 2008.
-    Speed matters.
-    @param h: apply this to each element of S
-    @param S: selection S_ij
-    @param: reversible fixation rates with log(F) stationary process
-    """
-    np.zeros
-    pass
-
-def get_Q(
-        ts, tv, syn, nonsyn, asym_compo,
-        log_mu, log_kappa, log_omega, log_nt_weights, fixation):
-    """
-    Notation is from Yang and Nielsen 2008.
+    The first group of args consists of precomputed ndarrays.
+    The second group is only the fixation function.
+    The third group consists of empirically (non-free) estimated parameters.
+    The fourth group depends only on free parameters.
     Speed matters.
     @param ts: indicator for transition
     @param tv: indicator for transversion
     @param syn: indicator for synonymous codons
     @param nonsyn: indicator for nonsynonymous codons
+    @param compo: site independent nucleotide composition per codon
     @param asym_compo: tensor from get_asym_compo function
+    @param h: fixation function
+    @param log_counts: empirically counted codons in the data set
     @param log_mu: free param for scaling
     @param log_kappa: free param for transition transversion rate distinction
     @param log_omega: free param for syn nonsyn rate distinction
     @param log_nt_weights: mostly free param array for mutation equilibrium
-    @param fixation: matrix of h(S_ij) = h(F_j - F_i)
-    @return: rate matrix Q
+    @return: transition matrix
     """
     mu = math.exp(log_mu)
     kappa = math.exp(log_kappa)
     omega = math.exp(log_omega)
-    return mu * (kappa * ts + tv) * (omega * nonsyn + syn) * np.exp(np.dot(
-            asym_compo, log_nt_weights)) * fixation
+    F = get_selection_F(log_counts, compo, log_nt_weights)
+    S = get_selection_S(F)
+    pre_Q = mu * (kappa * ts + tv) * (omega * nonsyn + syn) * np.exp(
+            np.dot(asym_compo, log_nt_weights)) * h(S)
+    Q = pre_Q - np.diag(np.sum(pre_Q, axis=1))
+    P = linalg.expm(Q)
+    return P
+
+def read_yang_alignments(codons, lines):
+    """
+    Read the file that Ziheng Yang put on his website.
+    For our purposes,
+    an alignment will be a list of [taxon_name, codon_index_sequence] pairs.
+    @param codons: sequence lowercase codons
+    @param lines: text lines of the alignment
+    @return: list of alignments
+    """
+    names = {'hg18', 'pantro2', 'rhemac2', 'mm8', 'rn4'}
+    # construct the inverse codon map
+    c_to_i = dict((c, i) for i, c in enumerate(codons))
+    # read the alignments
+    alignments = []
+    alignment = []
+    expected_len = None
+    for line in lines:
+        line = line.strip().lower()
+        if not line:
+            continue
+        elements = line.split()
+        if elements[0] in names:
+            seq = np.array([c_to_i[c] for c in elements[1:]], dtype=int)
+            if len(seq) * 3 != expected_len:
+                raise Exception((len(seq) * 3, expected_len))
+            alignment.append((elements[0], seq))
+        elif int(elements[0]) == len(names):
+            if len(elements) != 2:
+                raise Exception(elements)
+            expected_len = int(elements[1])
+            if alignment:
+                alignments.append(alignment)
+            alignment = []
+        else:
+            raise Exception(elements[0])
+    if alignment:
+        alignments.append(alignment)
+    return alignments
+
+def grouper(n, iterable, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
+
+def read_yang_mtdna_alignment(codons, lines):
+    c_to_i = dict((c, i) for i, c in enumerate(codons))
+    name = None
+    alignment = []
+    segments = []
+    for line in lines:
+        line = line.strip().lower()
+        if line in g_mtdna_names or not line:
+            if segments:
+                dna = ''.join(segments)
+                codons = zip(*[dna[i::3] for i in range(3)])
+                seq = np.array([c_to_i[''.join(c)] for c in codons], dtype=int)
+                alignment.append((name, seq))
+        if line in g_mtdna_names:
+            name = line
+            segments = []
+        elif not line:
+            name = None
+        else:
+            if name:
+                segments.append(line)
+    return alignment
+
+def get_empirical_summary(ncodons, alignments, t1, t2):
+    """
+    Get codon counts and substitution counts.
+    @param ncodons: allowed number of codons
+    @param alignments: from the get_yang_alignments function
+    @param t1: first taxon name
+    @param t2: second taxon name
+    @return: codon_counts, subs_counts
+    """
+    codon_counts = np.zeros(ncodons, dtype=int)
+    subs_counts = np.zeros((ncodons, ncodons), dtype=int)
+    for alignment in alignments:
+        d = dict(alignment)
+        for i, j in zip(d[t1], d[t2]):
+            codon_counts[i] += 1
+            codon_counts[j] += 1
+            subs_counts[i, j] += 1
+    return codon_counts, subs_counts
+
+def get_log_likelihood(P, v, subs_counts):
+    """
+    The stationary distribution of P is empirically derived.
+    It is proportional to the codon counts by construction.
+    @param P: a transition matrix using codon counts and free parameters
+    @param v: stationary distribution proportional to observed codon counts
+    @param subs_counts: observed substitution counts
+    """
+    return np.sum(subs_counts * np.log(P.T * v))
+
+def minimize_me(
+        theta,
+        subs_counts, log_counts, v,
+        h,
+        ts, tv, syn, nonsyn, compo, asym_compo,
+        ):
+    """
+    @param theta: length six unconstrained vector of free variables
+    """
+    #
+    # unpack theta
+    log_mu, log_kappa, log_omega, log_a, log_c, log_g = theta.tolist()
+    log_nt_weights = np.array([log_a, log_c, log_g, 0], dtype=float)
+    #
+    # construct the transition matrix
+    P = get_P(
+            ts, tv, syn, nonsyn, compo, asym_compo,
+            h,
+            log_counts,
+            log_mu, log_kappa, log_omega, log_nt_weights)
+    #
+    # return the neg log likelihood
+    return -get_log_likelihood(P, v, subs_counts)
 
 def main(args):
     #
-    # precompute some ndarrays
-    codons = enum_codons()
+    # Precompute some ndarrays
+    # according to properties of DNA and the genetic code.
+    all_codons = enum_codons()
+    codons = all_codons[:-3]
+    ts, tv = get_ts_tv(codons)
+    syn, nonsyn = get_syn_nonsyn(codons)
+    compo = get_compo(codons)
+    asym_compo = get_asym_compo(codons)
     #
     # check invariants of precomputed ndarrays
-    if len(codons) != 64:
+    if len(all_codons) != 64:
+        raise Exception
+    if len(codons) != 61:
+        raise Exception
+    if np.unique(ts).tolist() != [0, 1]:
+        raise Exception
+    if np.unique(tv).tolist() != [0, 1]:
         raise Exception
     #
     # check the genetic code for typos
@@ -220,9 +361,9 @@ def main(args):
     if len(table_codons) != len(set(table_codons)):
         raise Exception
     if set(codons) - set(table_codons):
-        raise Exception(set(codons) - set(table_codons))
-    if set(table_codons) - set(codons):
-        raise Exception(set(table_codons) - set(codons))
+        raise Exception(set(all_codons) - set(table_codons))
+    if set(table_codons) - set(all_codons):
+        raise Exception(set(table_codons) - set(all_codons))
     #
     # Check reversibility of h functions with respect to F,
     # in the notation of Yang and Nielsen 2008.
@@ -233,9 +374,46 @@ def main(args):
         log_ratio = np.log(fixation / fixation.T)
         if not np.allclose(S, log_ratio):
             raise Exception((S, log_ratio))
+    #
+    # read alignments from the file
+    with open(args.infile) as fin:
+        if args.mtdna:
+            alignments = [read_yang_mtdna_alignment(all_codons, fin)]
+        else:
+            alignments = read_yang_alignments(all_codons, fin)
+    print 'read', len(alignments), 'alignments'
+    print
+    #
+    # Extract the codon counts and the substitution counts.
+    # Then compute the empirical codon distribution and log codon counts.
+    if args.mtdna:
+        t1, t2 = g_mtdna_names
+    else:
+        t1, t2 = args.t1, args.t2
+    codon_counts, subs_counts = get_empirical_summary(64, alignments, t1, t2)
+    pseudocount = 1
+    codon_counts = pseudocount + codon_counts[:61]
+    subs_counts = subs_counts[:61, :61]
+    v = codon_counts / float(np.sum(codon_counts))
+    log_counts = np.log(codon_counts)
+    print 'codon counts:', codon_counts
+    print
+    #
+    #
+    h = get_fixation_genic
+    theta = np.zeros(6)
+    fmin_args = (
+            subs_counts, log_counts, v,
+            h,
+            ts, tv, syn, nonsyn, compo, asym_compo,
+            )
+    print optimize.fmin(minimize_me, theta, fmin_args, full_output=True)
+    print
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--mtdna', action='store_true',
+            help='read the mtdna file from the website of Ziheng Yang')
     parser.add_argument(
             '--infile',
             help='codon alignment input file using the format of Ziheng Yang',
