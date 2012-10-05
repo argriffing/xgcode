c235d1859b77956b683a89b4a5e9135bb8e758aa
a g <argriffi@brcdesk18.(none)>
2012-10-04 22:39:01 -0400

    vectorize the recessive disease fixation probability function and add a reversibility test

--- mle-recessive.py
+++ mle-recessive.py
@@ -5,6 +5,7 @@ It is for testing a pure recessive disease codon preference model
 against the genic model of Yang and Nielsen 2008.
 This script does not use so many biological libraries
 because for some reason I am adopting a more matlab-like style.
+All matrices in this script are ndarrays as opposed to actual python matrices.
 """
 
 import argparse
@@ -46,23 +47,29 @@ g_code = {
         }
 
 def get_fixation_genic(x):
-    if not x:
-        return 1.0
-    else:
-        return 1.0 / special.hyp1f1(1.0, 2.0, -x).real
+    """
+    This is an h function in the notation of Yang and Nielsen.
+    This should be applicable entrywise to an ndarray.
+    Speed is important.
+    """
+    return 1.0 / special.hyp1f1(1.0, 2.0, -x).real
 
 def get_fixation_recessive_disease(x):
-    if not x:
-        return 1.0
-    elif x < 0:
-        return 1.0 / special.hyp1f1(0.5, 1.5, -x).real
-    elif 0 < x:
-        return 1.0 / special.hyp1f1(1.0, 1.5, -x).real
+    """
+    This is an h function in the notation of Yang and Nielsen.
+    This should be applicable entrywise to an ndarray.
+    Speed is important.
+    """
+    xneg = np.clip(x, -np.inf, 0)
+    xpos = np.clip(x, 0, np.inf)
+    a = 1.0 / special.hyp1f1(0.5, 1.5, -xneg).real
+    b = 1.0 / special.hyp1f1(1.0, 1.5, -xpos).real
+    return a + b - 1.0
 
 def enum_codons():
     """
     Enumerate lower case codon strings with all three stop codons at the end.
-    Speed is not important.
+    Speed does not matter.
     @return: a list of 64 codons
     """
     codons = [''.join(triple) for triple in product('acgt', repeat=3)]
@@ -136,25 +143,73 @@ def get_asym_compo(codons):
                     a != b and b == nt))
     return asym_compo
 
-def get_selection(log_counts, compo, log_nt_weights):
+def get_selection_F(log_counts, compo, log_nt_weights):
     """
+    The F and S notation is from Yang and Nielsen 2008.
     Note that three of the four log nt weights are free parameters.
     One of the four log weights is zero and the other three
     are free parameters to be estimated jointly in the
     maximimum likelihood search,
     so this function is inside the optimization loop.
+    Speed matters.
     @param log_counts: logs of empirical codon counts
     @param compo: codon composition as defined in the get_compo function
     @param log_nt_weights: un-normalized log mutation process probabilities
-    @return: a matrix map from codon pair to selection difference
+    @return: a log selection for each codon, up to an additive constant
     """
-    #FIXME
+    return log_counts - np.dot(compo, log_nt_weights)
+
+def get_selection_S(F):
+    """
+    The F and S notation is from Yang and Nielsen 2008.
+    Speed matters.
+    @param F: a selection value for each codon, up to an additive constant
+    @return: selection differences F_j - F_i, also known as S_ij
+    """
+    e = np.ones_like(F)
+    return np.outer(e, F) - np.outer(F, e)
+
+def get_fixation(h, S):
+    """
+    Notation is from Yang and Nielsen 2008.
+    Speed matters.
+    @param h: apply this to each element of S
+    @param S: selection S_ij
+    @param: reversible fixation rates with log(F) stationary process
+    """
+    np.zeros
     pass
+
+def get_Q(
+        ts, tv, syn, nonsyn, asym_compo,
+        log_mu, log_kappa, log_omega, log_nt_weights, fixation):
+    """
+    Notation is from Yang and Nielsen 2008.
+    Speed matters.
+    @param ts: indicator for transition
+    @param tv: indicator for transversion
+    @param syn: indicator for synonymous codons
+    @param nonsyn: indicator for nonsynonymous codons
+    @param asym_compo: tensor from get_asym_compo function
+    @param log_mu: free param for scaling
+    @param log_kappa: free param for transition transversion rate distinction
+    @param log_omega: free param for syn nonsyn rate distinction
+    @param log_nt_weights: mostly free param array for mutation equilibrium
+    @param fixation: matrix of h(S_ij) = h(F_j - F_i)
+    @return: rate matrix Q
+    """
+    mu = math.exp(log_mu)
+    kappa = math.exp(log_kappa)
+    omega = math.exp(log_omega)
+    return mu * (kappa * ts + tv) * (omega * nonsyn + syn) * np.exp(np.dot(
+            asym_compo, log_nt_weights)) * fixation
 
 def main(args):
     #
-    # get a sequence of codons with stop codons at the end
+    # precompute some ndarrays
     codons = enum_codons()
+    #
+    # check invariants of precomputed ndarrays
     if len(codons) != 64:
         raise Exception
     #
@@ -168,6 +223,16 @@ def main(args):
         raise Exception(set(codons) - set(table_codons))
     if set(table_codons) - set(codons):
         raise Exception(set(table_codons) - set(codons))
+    #
+    # Check reversibility of h functions with respect to F,
+    # in the notation of Yang and Nielsen 2008.
+    for h in (get_fixation_genic, get_fixation_recessive_disease):
+        F = np.array([1.2, 2.3, 0, -1.1])
+        S = get_selection_S(F)
+        fixation = h(S)
+        log_ratio = np.log(fixation / fixation.T)
+        if not np.allclose(S, log_ratio):
+            raise Exception((S, log_ratio))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
