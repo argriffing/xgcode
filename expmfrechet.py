012347d6a0c0607e1afe0db2d23860ee071b6153
ag <argriffi@brcdesk18.statgen.ncsu.edu>
2012-10-11 22:43:52 -0400

    continue the quest for automatic differentiation through matrix exponential

--- expmfrechet.py
+++ expmfrechet.py
@@ -7,19 +7,66 @@ The magic constants in this module are tuned
 for double precision floating point computations.
 """
 
+import math
+
 import numpy as np
 from scipy import linalg
+
+# test matrices
+# http://people.sc.fsu.edu/~jburkardt/m_src/test_matrix_exponential/mexp_a.m
+g_test_matrices = [
+        np.array([
+            [1, 0],
+            [0, 2],
+            ], dtype=float),
+        np.array([
+            [1, 3],
+            [3, 2],
+            ], dtype=float),
+        np.array([
+            [0, 1],
+            [-39, -40],
+            ], dtype=float),
+        np.array([
+            [-49, 24],
+            [-64, 31],
+            ], dtype=float),
+        ]
+
+# expm of test matrices
+# http://people.sc.fsu.edu/~jburkardt/m_src/test_matrix_exponential/mexp_expa.m
+g_test_matrices_expm = [
+        np.array([
+            [2.718281828459046, 0],
+            [0, 7.389056098930650],
+            ], dtype=float),
+        np.array([
+            [39.322809708033859, 46.166301438885753],
+            [46.166301438885768, 54.711576854329110],
+            ], dtype=float),
+        np.array([
+            [0, 2.718281828459046],
+            [1.154822E-17, 2.718281828459046],
+            ], dtype=float),
+        np.array([
+            [-0.735759, 0.551819],
+            [-1.471518, 1.103638],
+            ], dtype=float),
+        ]
+
+
 
 # Coefficients of degree 13 Pade approximant.
 # From Algorithm 2.3 of Higham 2005,
 # The Scaling and Squaring Method for the Matrix Exponential Revisited.
 # b0 through b13
-btable = [
+btable = np.array([
         64752532480000, 32382376266240000, 7771770303897600,
         1187353796428800, 129060195264000, 10559470521600,
         670442572800, 33522128640, 1323241920,
         40840800, 960960, 16380, 182, 1,
-        ]
+        ], dtype=float)
+    
 b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13 = btable
 
 table61_theta = [
@@ -38,18 +85,6 @@ table61_l = [
         7.52e0, 8.53e0, 9.56e0, 1.06e1, 1.17e1,
         ]
 
-def alg611(X, m):
-    """
-    This is a naive transcription of definition (6.11).
-    It is a decomposition of a sum into odd and even parts.
-    """
-    U = np.zeros_like(X)
-    V = np.zeros_like(X)
-    for k in range(0, (m-1)//2 + 1):
-        X2k = linalg.matrix_power(X, 2*k)
-        U += btable[2*k + 1] * X2k
-        V += btable[2*k] * X2k
-    return np.dot(X, U), V
 
 def alg34(A, E, k):
     """
@@ -61,7 +96,7 @@ def alg34(A, E, k):
     elif k == 1:
         return E
     else:
-        return np.dot(alg34(k-1), A) + np.dot(linalg.matrix_power(A, k-1), E)
+        return np.dot(alg34(k-1), A) + np.dot(np.linalg.matrix_power(A, k-1), E)
 
 def alg612(A, E, m):
     """
@@ -76,9 +111,98 @@ def alg612(A, E, m):
         Lua += btable[2*k + 1] * M2k
         Lv += btable[2*k] * M2k
     for k in range(0, (m-1)//2 + 1):
-        Lub += btable[2*k + 1] * linalg.matrix_power(A, 2*k)
+        Lub += btable[2*k + 1] * np.linalg.matrix_power(A, 2*k)
     Lu = np.dot(A, Lua) + np.dot(E, Lub)
     return Lu, Lv
+
+def alg64_26_27(U, V, Lu, Lv):
+    """
+    This is a naive transcription of lines 26 and 27 of Algorithm 6.4.
+    The paper has a comment that the linear systems at lines 26 and 27
+    have the same coefficient matrix, so an LU factorization
+    can be computed once and reused.
+    """
+    R = linalg.solve(-U + V, U + V)
+    L = linalg.solve(-U + V, Lu + Lv + np.dot(Lu - Lv, R))
+    return R, L
+
+def alg611(X, m):
+    """
+    This is a naive transcription of definition (6.11).
+    It is a decomposition of a sum into odd and even parts.
+    """
+    U = np.zeros_like(X)
+    V = np.zeros_like(X)
+    for k in range(0, (m-1)//2 + 1):
+        X2k = np.linalg.matrix_power(X, 2*k)
+        U += btable[2*k + 1] * X2k
+        V += btable[2*k] * X2k
+    return np.dot(X, U), V
+
+def alg614(U, V):
+    return linalg.solve(-U + V, U + V)
+
+# from scipy
+def _pade13(A, ident):
+    b = (64764752532480000., 32382376266240000., 7771770303897600.,
+    1187353796428800., 129060195264000., 10559470521600., 670442572800.,
+    33522128640., 1323241920., 40840800., 960960., 16380., 182., 1.)
+    A2 = A.dot(A)
+    A4 = A2.dot(A2)
+    A6 = A4.dot(A2)
+    U = A.dot(A6.dot(b[13]*A6 + b[11]*A4 + b[9]*A2) + b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = A6.dot(b[12]*A6 + b[10]*A4 + b[8]*A2) + b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def hardcoded_13_13_pade_approximant(A, I):
+    """
+    Lines 17, 18, 19, 20 of algorithm 2.3 of Higham 2005.
+    """
+    A2 = np.linalg.matrix_power(A, 2)
+    A4 = np.linalg.matrix_power(A2, 2)
+    A6 = np.dot(A2, A4)
+    U = np.dot(
+            A,
+            np.dot(
+                A6,
+                b13*A6 + b11*A4 + b9*A2,
+                ) + b7*A6 + b5*A4 + b3*A2 + b1*I)
+    V = np.dot(
+            A6,
+            b12*A6 + b10*A4 + b8*A2,
+            ) + b6*A6 + b4*A4 + b2*A2 + b0*I
+    return U, V
+
+def alg61(A):
+    """
+    Scaling and squaring algorithm for exponential.
+    """
+    # XXX for now avoid stopping early
+    """
+    for m in (3, 5, 7, 9):
+        if linalg.norm(A, 1) < table61_theta[m]:
+            print 'using m =', m
+            U, V = alg611(A, m)
+            return alg614(U, V)
+    """
+    m = 13
+    s = int(math.ceil(math.log(linalg.norm(A, 1), 2) / table61_theta[m]))
+    A /= 2**s
+    """
+    print 'using m =', m
+    print 'using s =', s
+    U, V = alg611(A, m)
+    X = alg614(U, V)
+    """
+    U, V = hardcoded_13_13_pade_approximant(A, np.eye(A.shape[0]))
+    #U, V = _pade13(A, np.eye(A.shape[0]))
+    r13 = linalg.solve(-U + V, U + V)
+    return np.linalg.matrix_power(r13, 2**s)
+    """
+    for i in range(s):
+        X = np.linalg.matrix_power(X, 2)
+    return X
+    """
 
 def alg64(A, E):
     """
@@ -86,7 +210,15 @@ def alg64(A, E):
     Given A, E, this algorithm computes R and L by scaling and squaring.
     The matrices A and E are square nxn with complex entries.
     Matrix R is expm(A), and matrix L is Lexp(A, E) in their notation.
+    @return: R, L
     """
+    if A.ndim != 2:
+        raise Exception
+    n = A.shape[0]
+    if A.shape != (n, n):
+        raise Exception
+    if E.shape != (n, n):
+        raise Exception
     for m in (3, 5, 7, 9):
         if linalg.norm(A, 1) < table61_l[m]:
             # Evaluate U = um(A) and V = vm(A), using (6.11).
@@ -94,11 +226,45 @@ def alg64(A, E):
             # Evaluate Lu = Lum(A, E) and Lv = Lvm(A, E),
             # using (6.12) and (6.13).
             Lu, Lv = alg612(A, E, m)
-            #XXX
-
+            return alg64_26_27(U, V, Lu, Lv)
+    s = int(math.ceil(math.log(linalg.norm(A, 1) / table61_l[13], 2)))
+    A /= 2**s
+    E /= 2**s
+    A2 = np.linalg.matrix_power(A, 2)
+    A4 = np.linalg.matrix_power(A2, 2)
+    A6 = np.dot(A2, A4)
+    M2 = np.dot(A, E) + np.dot(E, A)
+    M4 = np.dot(A2, M2) + np.dot(M2, A2)
+    M6 = np.dot(A4, M2) + np.dot(M4, A2)
+    W1 = b13*A6 + b11*A4 + b9*A2
+    I = np.eye(n)
+    W2 = b7*A6 + b5*A4 + b3*A2 + b1*I
+    Z1 = b12*A1 + b10*A4 + b8*A2
+    Z2 = b6*A6 + b4*A4 + b2*A2 + b0*I
+    W = np.dot(A6, W1) + W2
+    U = np.dot(A, W)
+    V = np.dot(A6, Z1) + Z2
+    Lw1 = b13*M6 + b11*M4 + b9*M2
+    Lw2 = b7*M6 + b5*M4 + b3*M2
+    Lz1 = b12*M6 + b10*M4 + b8*M2
+    Lz2 = b6*M6 + b4*M4 + b2*M2
+    Lw = np.dot(A6, Lz1) + np.dot(M6, W1) + Lw2
+    Lu = np.dot(A, Lw) + np.dot(E, W)
+    Lv = np.dot(A6, Lz1) + np.dot(M6, Z1) + Lz2
+    R, L = alg64_26_27(U, V, Lu, Lv)
+    for k in range(1, s+1):
+        L = np.dot(R, L) + np.dot(L, R)
+        R = np.linalg.matrix_power(R, 2)
+    return R, L
 
 def main():
-    pass
+    for f in (linalg.expm, alg61):
+        for M, E in zip(g_test_matrices, g_test_matrices_expm):
+            print M
+            print E
+            print f(M)
+            print '---------------------'
+        print '==================================='
 
 if __name__ == '__main__':
     main()
---
+++ scipyexpm.py
@@ -0,0 +1,162 @@
+#
+# Authors: Travis Oliphant, March 2002
+#          Anthony Scopatz, August 2012 (Sparse Updates)
+#          Jake Vanderplas, August 2012 (Sparse Updates)
+#
+
+import math
+
+import numpy as np
+from scipy import linalg
+
+
+# test matrices
+# http://people.sc.fsu.edu/~jburkardt/m_src/test_matrix_exponential/mexp_a.m
+g_test_matrices = [
+        np.array([
+            [1, 0],
+            [0, 2],
+            ], dtype=float),
+        np.array([
+            [1, 3],
+            [3, 2],
+            ], dtype=float),
+        np.array([
+            [0, 1],
+            [-39, -40],
+            ], dtype=float),
+        np.array([
+            [-49, 24],
+            [-64, 31],
+            ], dtype=float),
+        ]
+
+# expm of test matrices
+# http://people.sc.fsu.edu/~jburkardt/m_src/test_matrix_exponential/mexp_expa.m
+g_test_matrices_expm = [
+        np.array([
+            [2.718281828459046, 0],
+            [0, 7.389056098930650],
+            ], dtype=float),
+        np.array([
+            [39.322809708033859, 46.166301438885753],
+            [46.166301438885768, 54.711576854329110],
+            ], dtype=float),
+        np.array([
+            [0, 2.718281828459046],
+            [1.154822E-17, 2.718281828459046],
+            ], dtype=float),
+        np.array([
+            [-0.735759, 0.551819],
+            [-1.471518, 1.103638],
+            ], dtype=float),
+        ]
+
+
+def expm(A):
+    """
+    Compute the matrix exponential using Pade approximation.
+
+    Parameters
+    ----------
+    A : array or sparse matrix, shape(M,M)
+        2D Array or Matrix (sparse or dense) to be exponentiated
+
+    Returns
+    -------
+    expA : array, shape(M,M)
+        Matrix exponential of A
+
+    References
+    ----------
+    N. J. Higham,
+    "The Scaling and Squaring Method for the Matrix Exponential Revisited",
+    SIAM. J. Matrix Anal. & Appl. 26, 1179 (2005).
+
+    """
+    n_squarings = 0
+
+    A_L1 = linalg.norm(A,1)
+    ident = np.eye(A.shape[0], A.shape[1], dtype=A.dtype)
+
+    if A_L1 < 1.495585217958292e-002:
+        U,V = _pade3(A, ident)
+    elif A_L1 < 2.539398330063230e-001:
+        U,V = _pade5(A, ident)
+    elif A_L1 < 9.504178996162932e-001:
+        U,V = _pade7(A, ident)
+    elif A_L1 < 2.097847961257068e+000:
+        U,V = _pade9(A, ident)
+    else:
+        maxnorm = 5.371920351148152
+        n_squarings = max(0, int(math.ceil(math.log(A_L1 / maxnorm, 2))))
+        A /= 2**n_squarings
+        U, V = _pade13(A, ident)
+    print U, V
+    R = linalg.solve(-U + V, U + V)
+    for i in range(n_squarings):
+        R = R.dot(R)
+    return R
+
+# implementation of Pade approximations of various degree using the algorithm presented in [Higham 2005]
+# These should apply to both dense and sparse matricies.
+# ident is the identity matrix, which matches A in being sparse or dense.
+def _pade3(A, ident):
+    b = (120., 60., 12., 1.)
+    A2 = A.dot(A)
+    U = A.dot(b[3]*A2 + b[1]*ident)
+    V = b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade5(A, ident):
+    b = (30240., 15120., 3360., 420., 30., 1.)
+    A2 = A.dot(A)
+    A4 = A2.dot(A2)
+    U = A.dot(b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade7(A, ident):
+    b = (17297280., 8648640., 1995840., 277200., 25200., 1512., 56., 1.)
+    A2 = A.dot(A)
+    A4 = A2.dot(A2)
+    A6 = A4.dot(A2)
+    U = A.dot(b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade9(A, ident):
+    b = (17643225600., 8821612800., 2075673600., 302702400., 30270240.,
+                2162160., 110880., 3960., 90., 1.)
+    A2 = A.dot(A)
+    A4 = A2.dot(A2)
+    A6 = A4.dot(A2)
+    A8 = A6.dot(A2)
+    U = A.dot(b[9]*A8 + b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = b[8]*A8 + b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade13(A, ident):
+    b = (64764752532480000., 32382376266240000., 7771770303897600.,
+    1187353796428800., 129060195264000., 10559470521600., 670442572800.,
+    33522128640., 1323241920., 40840800., 960960., 16380., 182., 1.)
+    A2 = A.dot(A)
+    A4 = A2.dot(A2)
+    A6 = A4.dot(A2)
+    U = A.dot(A6.dot(b[13]*A6 + b[11]*A4 + b[9]*A2) + b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = A6.dot(b[12]*A6 + b[10]*A4 + b[8]*A2) + b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+
+def main():
+    for f in (linalg.expm, expm):
+        for M, E in zip(g_test_matrices, g_test_matrices_expm):
+            print M
+            print E
+            print f(M)
+            print '---------------------'
+        print '==================================='
+
+if __name__ == '__main__':
+    main()
+
