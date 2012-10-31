6023b425bea0ae8af962c573ffae9b40e824d9b1
alex <argriffi@ncsu.edu>
2012-10-31 15:04:11 -0400

    finish the script that constructs the recessivity/dominance rate matrix

--- 20121031a.py
+++ 20121031a.py
@@ -1,10 +1,10 @@
 """
-Construct a nucleotide rate matrix with recessivity-inspired parameters. [UNFINISHED]
+Construct a nucleotide rate matrix with recessivity-inspired parameters.
 
 This model of nucleotide evolution is traditional in the sense that it is
 a time-reversible 4-state continuous-time Markov process,
 and its equilibrium state distribution corresponds
-directly to a subset of model parameters.
+directly to a subset of the model parameters.
 It is also traditional in the sense that there is a parameter that
 can be adjusted to simultaneously rescale all of the rates by the same amount.
 But this model also has two parameters which are inspired by
@@ -16,30 +16,35 @@ is explained in a 1957 paper by Kimura.
 Perhaps eventually these kinds of parameters may make their way into
 more complicated phylogenetic models of evolution that try to model
 the interaction of many population-genetic concepts like mutation, fixation,
-drift, selection, dominance/recessivity, migration, population size, etc.
+drift, natural selection, dominance/recessivity, migration,
+population size, generation time, etc.
 but for now I am looking at only a 4-state model of nucleotide evolution
 with only a couple of parameters that do not correspond directly to
 the overall substitution rate or to the equilibrium nucleotide distribution.
-All seven listed parameters are inconveniently transformed in a way that
-allows any positive or negative number to be used for any parameter.
+All seven options listed below are inconveniently transformed in a way that
+allows any positive or negative number to be used for any option.
 The d parameter is better known as 2*h - 1, and is defined with respect
 to a new mutant that is more fit than the previously fixed allele.
+Note that if h' = 1-h then d' = -d.
 The k parameter controls the sharpness of the recessivity/dominance flip
 as a selection parameter changes sign;
 when k is a large positive number this change is very abrupt,
-whereas the change is nearly linear when k is a negative number with large
-absolute value.
+whereas a negative value of k with large absolute value means that
+selection is nearly genic for a wider range of selection values.
+When the log expected rate is zero,
+then the rate matrix will be scaled such that the process has
+one expected nucleotide substitution per time unit.
+When either d=0 or k<<0 this model should reduce to the
+rate matrix constructed using the neutral Kimura-inspired parameterization
+under a Jukes-Cantor mutation model.
 """
 
 from StringIO import StringIO
 import math
-import cmath
-import random
 
 import numpy
 import scipy
 import scipy.special
-import algopy
 
 import Form
 import FormOut
@@ -54,7 +59,7 @@ def get_form():
             Form.Float('C_log_weight', 'C log weight', 1.2),
             Form.Float('G_log_weight', 'G log weight', 0.0),
             Form.Float('T_log_weight', 'T log weight', 0.3),
-            Form.Float('d', 'dominance of fitter allele d', 0.4),
+            Form.Float('d', 'dominance d of fitter allele', 0.4),
             Form.Float('k', 'Kacser and Burn effect parameter k', 2.0),
             Form.Float('log_rate', 'log expected rate', 0),
             ]
@@ -62,46 +67,73 @@ def get_form():
 def get_form_out():
     return FormOut.Report()
 
+def get_fixation_unconstrained(S, d, k):
+    D = d * numpy.tanh(numpy.exp(k)*S)
+    H = numpy.zeros_like(S)
+    for i in range(H.shape[0]):
+        for j in range(H.shape[1]):
+            H[i, j] = 1. / kimrecessive.denom_piecewise(
+                    0.5*S[i, j], D[i, j])
+    return H
+
+def create_pre_rate_matrix(fs):
+    """
+    The returned rate matrix is not diagonally adjusted or rate-scaled.
+    """
+
+    # Construct the stationary distribution
+    log_v = numpy.array([
+        fs.A_log_weight,
+        fs.C_log_weight,
+        fs.G_log_weight,
+        fs.T_log_weight,
+        ])
+    v = numpy.exp(log_v)
+    v /= numpy.sum(v)
+    
+    # Construct F and S in the Yang-Nielsen 2008 notation.
+    F = log_v
+    e = numpy.ones_like(F)
+    S = numpy.outer(e, F) - numpy.outer(F, e)
+
+    # Compute the recessivity component of the rate matrix.
+    H = get_fixation_unconstrained(S, fs.d, fs.k)
+
+    # Construct and return the pre rate matrix and stationary distribution.
+    pre_Q = H
+    return H, v
+
+
 def get_response_content(fs):
-    #numpy.set_printoptions(linewidth=200)
+    numpy.set_printoptions(linewidth=200)
     out = StringIO()
     #
-    print >> out, 'generating random selection and dominance...'
+    pre_Q, v = create_pre_rate_matrix(fs)
+    Q = pre_Q - numpy.diag(numpy.sum(pre_Q, axis=1))
+    observed_rate = -numpy.dot(v, numpy.diag(Q))
+    requested_rate = numpy.exp(fs.log_rate)
+    Q *= (requested_rate / observed_rate)
+    #
+    print >> out, 'stationary distribution:'
+    print >> out, v
     print >> out
-    for i in range(10):
-        #c = random.random()
-        #d = random.random()
-        c = random.gauss(0, 1)
-        d = random.gauss(0, 0.01)
-        print >> out, 'c:', c
-        print >> out, 'd:', d
-        print >> out
-        print >> out, 'method a:'
-        print >> out, kimura_1957_54_denominator_analytic(c, d)
-        print >> out
-        print >> out, 'method b:'
-        print >> out, kimura_1957_54_denominator_analytic_b(c, d)
-        print >> out
-        print >> out, 'asymptotic:'
-        print >> out, kimura_1957_54_denominator_analytic_asymptotic(c, d)
-        print >> out
-        print >> out, 'asymptotic, genic:'
-        print >> out, kimura_1957_54_denominator_analytic_asymptotic(c, 0)
-        print >> out
-        print >> out, 'explicitly genic:'
-        print >> out, kimura_genic(c)
-        print >> out
-        print >> out
-    print >> out, 'asymptotic(c=0.5, d=0):'
-    print >> out, kimura_1957_54_denominator_analytic_asymptotic(0.5, 0)
+    print >> out, 'rate matrix:'
+    print >> out, Q
     print >> out
-    #print >> out, 'general_purpose(c=1, d=0):'
-    #print >> out, kimura_1957_54_denominator_analytic_b(1, 0)
-    #print >> out, '(division by zero error)'
-    #print >> out
-    print >> out, 'general_purpose(c=0, d=0.5):'
-    print >> out, kimura_1957_54_denominator_analytic_b(0, 0.5)
+    print >> out, 'pi_i Q_ij (to check reversibility):'
+    print >> out, numpy.dot(numpy.diag(v), Q)
     print >> out
+    """
+    print >> out, '<html><head></head><body>'
+    print >> out, '<pre>'
+    print >> out, 'stationary distribution:'
+    print >> out, v
+    print >> out, '</pre>'
+    print >> out
+    print >> out, '<table>'
+    print >> out, '</table>'
+    print >> out, '</body></html>'
+    """
     #
     return out.getvalue()
 
